0a0937a98ec190dbd93e353126118170dad82bb0
Seth Butler <sbsoftware@protonmail.com>
2022-02-13 14:05:33 -0500

    Add tests for ClientRequestHandler and utilize resp reader and writer

--- server/src/main/java/dev/sbutler/bitflask/server/client_processing/ClientRequestHandler.java
+++ server/src/main/java/dev/sbutler/bitflask/server/client_processing/ClientRequestHandler.java
@@ -1,11 +1,10 @@
 package dev.sbutler.bitflask.server.client_processing;
 
+import dev.sbutler.bitflask.resp.utilities.RespReader;
+import dev.sbutler.bitflask.resp.utilities.RespWriter;
 import dev.sbutler.bitflask.server.command_processing.CommandProcessor;
 import dev.sbutler.bitflask.server.storage.Storage;
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.Socket;
 
 public class ClientRequestHandler implements Runnable {
@@ -14,26 +13,28 @@ public class ClientRequestHandler implements Runnable {
 
   private final Socket socket;
   private final ClientMessageProcessor clientMessageProcessor;
+  private boolean shouldContinueRunning = true;
 
   public ClientRequestHandler(Socket socket, Storage storage) throws IOException {
     this.socket = socket;
-    this.clientMessageProcessor = new ClientMessageProcessor(new CommandProcessor(storage),
-        socket.getInputStream(), socket.getOutputStream());
+    this.clientMessageProcessor = new ClientMessageProcessor(
+        new CommandProcessor(storage),
+        new RespReader(socket.getInputStream()),
+        new RespWriter(socket.getOutputStream())
+    );
   }
 
   @Override
   public void run() {
-    while (!Thread.interrupted()) {
-      boolean processedSuccessfully = clientMessageProcessor.processNextMessage();
-      if (!processedSuccessfully) {
-        break;
-      }
+    while (!Thread.interrupted() && shouldContinueRunning) {
+      shouldContinueRunning = clientMessageProcessor.processNextMessage();
     }
     this.close();
   }
 
   public void close() {
     try {
+      shouldContinueRunning = false;
       socket.close();
       System.out.println(TERMINATING_CONNECTION);
     } catch (IOException e) {
--- server/src/test/java/dev/sbutler/bitflask/server/client_processing/ClientRequestHandlerTest.java
+++ server/src/test/java/dev/sbutler/bitflask/server/client_processing/ClientRequestHandlerTest.java
@@ -1,74 +1,52 @@
 package dev.sbutler.bitflask.server.client_processing;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
+import dev.sbutler.bitflask.server.storage.Storage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 public class ClientRequestHandlerTest {
 
-//  private final Socket socket = mock(Socket.class);
-//  private final Storage storage = mock(Storage.class);
-//  private ByteArrayInputStream byteArrayInputStream;
-//  private ByteArrayOutputStream byteArrayOutputStream;
-//
-//  void prepareSocketMocks(byte[] inputStreamBytes) throws IOException {
-//    byteArrayOutputStream = new ByteArrayOutputStream();
-//    when(socket.getOutputStream()).thenReturn(byteArrayOutputStream);
-//    byteArrayInputStream = new ByteArrayInputStream(inputStreamBytes);
-//    when(socket.getInputStream()).thenReturn(byteArrayInputStream);
-//  }
-//
-//  @Test
-//  void processRequest_success() throws IOException {
-//    // one second read an EOFException will be thrown since the input stream was exhausted
-//    // This is fine and allows the call to return
-//    RespType<?> clientMessage = new RespArray(List.of(
-//        new RespBulkString("ping")
-//    ));
-//    RespType<?> expectedResponse = new RespBulkString("pong");
-//    prepareSocketMocks(clientMessage.getEncodedBytes());
-//
-//    RequestHandler requestHandler = new RequestHandler(socket, storage);
-//    requestHandler.run();
-//
-//    byte[] response = byteArrayOutputStream.toByteArray();
-//    assertArrayEquals(expectedResponse.getEncodedBytes(), response);
-//  }
-//
-//  @Test
-//  void processRequest_EOFException() throws IOException {
-//    prepareSocketMocks(new byte[0]);
-//
-//    RequestHandler requestHandler = new RequestHandler(socket, storage);
-//    byteArrayInputStream.close();
-//    requestHandler.run();
-//    verify(socket, times(1)).close();
-//  }
-//
-//  @Test
-//  void processRequest_IOException() throws IOException {
-//    RespType<?> clientMessage = new RespArray(List.of(
-//        new RespBulkString("ping")
-//    ));
-//    prepareSocketMocks(clientMessage.getEncodedBytes());
-//
-//    RequestHandler requestHandler = new RequestHandler(socket, storage);
-//    requestHandler.run();
-//    verify(socket, times(1)).close();
-//  }
-//
-//  @Test
-//  void getServerResponseToClient_IllegalArgumentException() throws IOException {
-//    RespType<?> invalidClientMessage = new RespInteger(1);
-//    prepareSocketMocks(invalidClientMessage.getEncodedBytes());
-//
-//    RequestHandler requestHandler = new RequestHandler(socket, storage);
-//    requestHandler.run();
-//
-//    String response = byteArrayOutputStream.toString();
-//    assertTrue(response.contains("Invalid"));
-//  }
+  @Mock
+  Socket socket;
+  @Mock
+  Storage storage;
 
+  @Test
+  void start_IOException() throws IOException {
+    doReturn(new ByteArrayInputStream(new byte[0])).when(socket).getInputStream();
+    doReturn(new ByteArrayOutputStream()).when(socket).getOutputStream();
+    ClientRequestHandler clientRequestHandler = new ClientRequestHandler(socket, storage);
+    clientRequestHandler.run();
+    verify(socket, times(1)).close();
+  }
+
+  @Test
+  void close_IOException() throws IOException {
+    doReturn(new ByteArrayInputStream(new byte[0])).when(socket).getInputStream();
+    doReturn(new ByteArrayOutputStream()).when(socket).getOutputStream();
+    doThrow(new IOException("Test: failure to close socket")).when(socket).close();
+
+    try {
+      ClientRequestHandler clientRequestHandler = new ClientRequestHandler(socket, storage);
+      clientRequestHandler.run();
+    } catch (Exception e) {
+      fail();
+    }
+
+    verify(socket, times(1)).close();
+  }
 }
