0cbf1057612ff94b4dc2d161bae2ba060b0d37c2
Seth Butler <sbsoftware@protonmail.com>
2022-03-02 11:17:08 -0500

    resp: add tests for RespNetworkModule

--- resp/src/test/java/dev/sbutler/bitflask/resp/network/RespNetworkModuleTest.java
+++ resp/src/test/java/dev/sbutler/bitflask/resp/network/RespNetworkModuleTest.java
@@ -1,8 +1,39 @@
 package dev.sbutler.bitflask.resp.network;
 
+import static org.mockito.Mockito.mock;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.Provides;
+import dev.sbutler.bitflask.resp.network.reader.RespReader;
+import dev.sbutler.bitflask.resp.network.writer.RespWriter;
+import java.io.InputStream;
+import java.io.OutputStream;
+import org.junit.jupiter.api.Test;
+
 public class RespNetworkModuleTest {
 
-  private final RespNetworkModule respNetworkModule = new RespNetworkModule();
+  static class MockStreamProvider extends AbstractModule {
 
-  // todo: implement tests
+    @Provides
+    InputStream providesInputStream() {
+      return mock(InputStream.class);
+    }
+
+    @Provides
+    OutputStream provideOutputStream() {
+      return mock(OutputStream.class);
+    }
+  }
+
+  @Test
+  void configure() {
+    Injector injector = Guice.createInjector(
+        new MockStreamProvider(),
+        new RespNetworkModule()
+    );
+    injector.getProvider(RespReader.class);
+    injector.getProvider(RespWriter.class);
+  }
 }
