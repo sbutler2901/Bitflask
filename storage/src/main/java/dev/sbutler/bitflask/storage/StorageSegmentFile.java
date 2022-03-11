2d3848a743f528beb0895fb39286bf5ff931a265
Seth Butler <sbsoftware@protonmail.com>
2022-03-11 14:31:23 -0500

    storage: create StorageSegmentFile to for managing file responsibilities when reading and writing

---
+++ storage/src/main/java/dev/sbutler/bitflask/storage/StorageSegmentFile.java
@@ -0,0 +1,59 @@
+package dev.sbutler.bitflask.storage;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.AsynchronousFileChannel;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.ThreadPoolExecutor;
+
+class StorageSegmentFile {
+
+  private static final String DEFAULT_SEGMENT_FILE_PATH = "store/segment%d.txt";
+  private static final StandardOpenOption[] fileOptions = {StandardOpenOption.CREATE,
+      StandardOpenOption.READ,
+      StandardOpenOption.WRITE, StandardOpenOption.TRUNCATE_EXISTING};
+  private static final Set<StandardOpenOption> fileChannelOptions = new HashSet<>(
+      Arrays.asList(fileOptions));
+
+  private final AsynchronousFileChannel segmentFileChannel;
+
+  public StorageSegmentFile(ThreadPoolExecutor threadPoolExecutor, int segmentIndex)
+      throws IOException {
+    Path newSegmentFilePath = Paths
+        .get(String.format(DEFAULT_SEGMENT_FILE_PATH, segmentIndex));
+    segmentFileChannel = AsynchronousFileChannel
+        .open(newSegmentFilePath, fileChannelOptions, threadPoolExecutor);
+  }
+
+  void write(byte[] data, long fileOffset) throws IOException {
+    Future<Integer> writeFuture = segmentFileChannel.write(ByteBuffer.wrap(data), fileOffset);
+    try {
+      writeFuture.get();
+    } catch (InterruptedException | ExecutionException e) {
+      String message = String.format("Failed to write the provided date at fileOffset [%d]",
+          fileOffset);
+      throw new IOException(message, e);
+    }
+  }
+
+  byte[] read(int readLength, long fileOffset) throws IOException {
+    ByteBuffer readBytesBuffer = ByteBuffer.allocate(readLength);
+    Future<Integer> readFuture = segmentFileChannel.read(readBytesBuffer, fileOffset);
+
+    try {
+      readFuture.get();
+    } catch (InterruptedException | ExecutionException e) {
+      String message = String.format("Failed to read the date at fileOffset [%d]", fileOffset);
+      throw new IOException(message, e);
+    }
+
+    return readBytesBuffer.array();
+  }
+}
---
+++ storage/src/test/java/dev/sbutler/bitflask/storage/StorageSegmentFileTest.java
@@ -0,0 +1,112 @@
+package dev.sbutler.bitflask.storage;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.AsynchronousFileChannel;
+import java.nio.file.Path;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.ThreadPoolExecutor;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+
+public class StorageSegmentFileTest {
+
+  @Test
+  void write() throws IOException {
+    ThreadPoolExecutor threadPoolExecutor = mock(ThreadPoolExecutor.class);
+    try (MockedStatic<AsynchronousFileChannel> asynchronousFileChannelMockedStatic = mockStatic(
+        AsynchronousFileChannel.class)) {
+      AsynchronousFileChannel asynchronousFileChannel = mock(AsynchronousFileChannel.class);
+      asynchronousFileChannelMockedStatic.when(
+              () -> AsynchronousFileChannel.open(any(Path.class), any(), any(ThreadPoolExecutor.class)))
+          .thenReturn(asynchronousFileChannel);
+
+      StorageSegmentFile storageSegmentFile = new StorageSegmentFile(threadPoolExecutor, 0);
+
+      doReturn(mock(Future.class)).when(asynchronousFileChannel)
+          .write(any(ByteBuffer.class), anyLong());
+      storageSegmentFile.write(new byte[]{'a'}, 0L);
+      verify(asynchronousFileChannel, times(1)).write(any(ByteBuffer.class), anyLong());
+    }
+  }
+
+  @Test
+  @SuppressWarnings("unchecked")
+  void write_exception() throws IOException, ExecutionException, InterruptedException {
+    ThreadPoolExecutor threadPoolExecutor = mock(ThreadPoolExecutor.class);
+    try (MockedStatic<AsynchronousFileChannel> asynchronousFileChannelMockedStatic = mockStatic(
+        AsynchronousFileChannel.class)) {
+      AsynchronousFileChannel asynchronousFileChannel = mock(AsynchronousFileChannel.class);
+      asynchronousFileChannelMockedStatic.when(
+              () -> AsynchronousFileChannel.open(any(Path.class), any(), any(ThreadPoolExecutor.class)))
+          .thenReturn(asynchronousFileChannel);
+
+      StorageSegmentFile storageSegmentFile = new StorageSegmentFile(threadPoolExecutor, 0);
+
+      Future<Integer> writeFuture = mock(Future.class);
+      doReturn(writeFuture).when(asynchronousFileChannel)
+          .write(any(ByteBuffer.class), anyLong());
+      doThrow(new InterruptedException("test: interruptException")).when(writeFuture).get();
+
+      assertThrows(IOException.class,
+          () -> storageSegmentFile.write(new byte[]{'a'}, 0L));
+      verify(asynchronousFileChannel, times(1)).write(any(ByteBuffer.class), anyLong());
+    }
+  }
+
+  @Test
+  @SuppressWarnings("unchecked")
+  void read() throws IOException {
+    ThreadPoolExecutor threadPoolExecutor = mock(ThreadPoolExecutor.class);
+    try (MockedStatic<AsynchronousFileChannel> asynchronousFileChannelMockedStatic = mockStatic(
+        AsynchronousFileChannel.class)) {
+      AsynchronousFileChannel asynchronousFileChannel = mock(AsynchronousFileChannel.class);
+      asynchronousFileChannelMockedStatic.when(
+              () -> AsynchronousFileChannel.open(any(Path.class), any(), any(ThreadPoolExecutor.class)))
+          .thenReturn(asynchronousFileChannel);
+
+      StorageSegmentFile storageSegmentFile = new StorageSegmentFile(threadPoolExecutor, 0);
+
+      Future<Integer> readFuture = mock(Future.class);
+      doReturn(readFuture).when(asynchronousFileChannel).read(any(ByteBuffer.class), anyLong());
+
+      byte[] result = storageSegmentFile.read(0, 0L);
+      verify(asynchronousFileChannel, times(1)).read(any(ByteBuffer.class), anyLong());
+      assertEquals(0, result.length);
+    }
+  }
+
+  @Test
+  @SuppressWarnings("unchecked")
+  void read_exception() throws IOException, ExecutionException, InterruptedException {
+    ThreadPoolExecutor threadPoolExecutor = mock(ThreadPoolExecutor.class);
+    try (MockedStatic<AsynchronousFileChannel> asynchronousFileChannelMockedStatic = mockStatic(
+        AsynchronousFileChannel.class)) {
+      AsynchronousFileChannel asynchronousFileChannel = mock(AsynchronousFileChannel.class);
+      asynchronousFileChannelMockedStatic.when(
+              () -> AsynchronousFileChannel.open(any(Path.class), any(), any(ThreadPoolExecutor.class)))
+          .thenReturn(asynchronousFileChannel);
+
+      StorageSegmentFile storageSegmentFile = new StorageSegmentFile(threadPoolExecutor, 0);
+
+      Future<Integer> readFuture = mock(Future.class);
+      doReturn(readFuture).when(asynchronousFileChannel).read(any(ByteBuffer.class), anyLong());
+      doThrow(new InterruptedException("test: interruptException")).when(readFuture).get();
+
+      assertThrows(IOException.class, () -> storageSegmentFile.read(0, 0L));
+      verify(asynchronousFileChannel, times(1)).read(any(ByteBuffer.class), anyLong());
+    }
+  }
+}
