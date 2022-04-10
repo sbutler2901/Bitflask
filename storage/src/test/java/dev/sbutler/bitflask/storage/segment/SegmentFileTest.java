ff4b630987181019fdff606c3534838f43feedb2
Seth Butler <sbsoftware@protonmail.com>
2022-04-10 14:36:29 -0600

    storage: update SegmentFile tests

--- storage/src/test/java/dev/sbutler/bitflask/storage/segment/SegmentFileTest.java
+++ storage/src/test/java/dev/sbutler/bitflask/storage/segment/SegmentFileTest.java
@@ -7,106 +7,74 @@ import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.mockStatic;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.AsynchronousFileChannel;
-import java.nio.file.Path;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
 import org.junit.jupiter.api.Test;
-import org.mockito.MockedStatic;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 public class SegmentFileTest {
+
+  @InjectMocks
+  SegmentFile segmentFile;
+  @Mock
+  AsynchronousFileChannel asynchronousFileChannel;
 
   @Test
   void write() throws IOException {
-    ExecutorService executorService = mock(ExecutorService.class);
-    try (MockedStatic<AsynchronousFileChannel> asynchronousFileChannelMockedStatic = mockStatic(
-        AsynchronousFileChannel.class)) {
-      AsynchronousFileChannel asynchronousFileChannel = mock(AsynchronousFileChannel.class);
-      asynchronousFileChannelMockedStatic.when(
-              () -> AsynchronousFileChannel.open(any(Path.class), any(), any(ExecutorService.class)))
-          .thenReturn(asynchronousFileChannel);
-
-      SegmentFile segmentFile = new SegmentFile(executorService);
-
-      doReturn(mock(Future.class)).when(asynchronousFileChannel)
-          .write(any(ByteBuffer.class), anyLong());
-      segmentFile.write(new byte[]{'a'}, 0L);
-      verify(asynchronousFileChannel, times(1)).write(any(ByteBuffer.class), anyLong());
-    }
+    doReturn(mock(Future.class)).when(asynchronousFileChannel)
+        .write(any(ByteBuffer.class), anyLong());
+    segmentFile.write(new byte[]{'a'}, 0L);
+    verify(asynchronousFileChannel, times(1)).write(any(ByteBuffer.class), anyLong());
   }
 
   @Test
   @SuppressWarnings("unchecked")
-  void write_exception() throws IOException, ExecutionException, InterruptedException {
-    ExecutorService executorService = mock(ExecutorService.class);
-    try (MockedStatic<AsynchronousFileChannel> asynchronousFileChannelMockedStatic = mockStatic(
-        AsynchronousFileChannel.class)) {
-      AsynchronousFileChannel asynchronousFileChannel = mock(AsynchronousFileChannel.class);
-      asynchronousFileChannelMockedStatic.when(
-              () -> AsynchronousFileChannel.open(any(Path.class), any(), any(ExecutorService.class)))
-          .thenReturn(asynchronousFileChannel);
+  void write_exception() throws ExecutionException, InterruptedException {
+    Future<Integer> writeFuture = mock(Future.class);
+    doReturn(writeFuture).when(asynchronousFileChannel)
+        .write(any(ByteBuffer.class), anyLong());
+    doThrow(new InterruptedException("test: interruptException")).when(writeFuture).get();
 
-      SegmentFile segmentFile = new SegmentFile(executorService);
-
-      Future<Integer> writeFuture = mock(Future.class);
-      doReturn(writeFuture).when(asynchronousFileChannel)
-          .write(any(ByteBuffer.class), anyLong());
-      doThrow(new InterruptedException("test: interruptException")).when(writeFuture).get();
-
-      assertThrows(IOException.class,
-          () -> segmentFile.write(new byte[]{'a'}, 0L));
-      verify(asynchronousFileChannel, times(1)).write(any(ByteBuffer.class), anyLong());
-    }
+    assertThrows(IOException.class,
+        () -> segmentFile.write(new byte[]{'a'}, 0L));
+    verify(asynchronousFileChannel, times(1)).write(any(ByteBuffer.class), anyLong());
   }
 
   @Test
   @SuppressWarnings("unchecked")
   void read() throws IOException {
-    ExecutorService executorService = mock(ExecutorService.class);
-    try (MockedStatic<AsynchronousFileChannel> asynchronousFileChannelMockedStatic = mockStatic(
-        AsynchronousFileChannel.class)) {
-      AsynchronousFileChannel asynchronousFileChannel = mock(AsynchronousFileChannel.class);
-      asynchronousFileChannelMockedStatic.when(
-              () -> AsynchronousFileChannel.open(any(Path.class), any(), any(ExecutorService.class)))
-          .thenReturn(asynchronousFileChannel);
+    Future<Integer> readFuture = mock(Future.class);
+    doReturn(readFuture).when(asynchronousFileChannel).read(any(ByteBuffer.class), anyLong());
 
-      SegmentFile segmentFile = new SegmentFile(executorService);
-
-      Future<Integer> readFuture = mock(Future.class);
-      doReturn(readFuture).when(asynchronousFileChannel).read(any(ByteBuffer.class), anyLong());
-
-      byte[] result = segmentFile.read(0, 0L);
-      verify(asynchronousFileChannel, times(1)).read(any(ByteBuffer.class), anyLong());
-      assertEquals(0, result.length);
-    }
+    byte[] result = segmentFile.read(0, 0L);
+    verify(asynchronousFileChannel, times(1)).read(any(ByteBuffer.class), anyLong());
+    assertEquals(0, result.length);
   }
 
   @Test
   @SuppressWarnings("unchecked")
-  void read_exception() throws IOException, ExecutionException, InterruptedException {
-    ExecutorService executorService = mock(ExecutorService.class);
-    try (MockedStatic<AsynchronousFileChannel> asynchronousFileChannelMockedStatic = mockStatic(
-        AsynchronousFileChannel.class)) {
-      AsynchronousFileChannel asynchronousFileChannel = mock(AsynchronousFileChannel.class);
-      asynchronousFileChannelMockedStatic.when(
-              () -> AsynchronousFileChannel.open(any(Path.class), any(), any(ExecutorService.class)))
-          .thenReturn(asynchronousFileChannel);
+  void read_exception() throws ExecutionException, InterruptedException {
+    Future<Integer> readFuture = mock(Future.class);
+    doReturn(readFuture).when(asynchronousFileChannel).read(any(ByteBuffer.class), anyLong());
+    doThrow(new InterruptedException("test: interruptException")).when(readFuture).get();
 
-      SegmentFile segmentFile = new SegmentFile(executorService);
+    assertThrows(IOException.class, () -> segmentFile.read(0, 0L));
+    verify(asynchronousFileChannel, times(1)).read(any(ByteBuffer.class), anyLong());
+  }
 
-      Future<Integer> readFuture = mock(Future.class);
-      doReturn(readFuture).when(asynchronousFileChannel).read(any(ByteBuffer.class), anyLong());
-      doThrow(new InterruptedException("test: interruptException")).when(readFuture).get();
-
-      assertThrows(IOException.class, () -> segmentFile.read(0, 0L));
-      verify(asynchronousFileChannel, times(1)).read(any(ByteBuffer.class), anyLong());
-    }
+  @Test
+  void size() throws IOException {
+    doReturn(0L).when(asynchronousFileChannel).size();
+    assertEquals(0L, segmentFile.size());
   }
 }
