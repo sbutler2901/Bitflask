2309113619d83b989d4470fb78b137dc16de6460
Seth Butler <sbsoftware@protonmail.com>
2022-06-19 22:16:36 -0600

    storage: add tests for SegmentCompactorProvider

---
+++ storage/src/test/java/dev/sbutler/bitflask/storage/segment/compactor/SegmentCompactorProviderTest.java
@@ -0,0 +1,30 @@
+package dev.sbutler.bitflask.storage.segment.compactor;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import dev.sbutler.bitflask.storage.segment.SegmentFactory;
+import java.util.concurrent.ExecutorService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class SegmentCompactorProviderTest {
+
+  @InjectMocks
+  SegmentCompactorProvider segmentCompactorProvider;
+  @Mock
+  ExecutorService executorService;
+  @Mock
+  SegmentFactory segmentFactory;
+
+  @Test
+  void get() {
+    SegmentCompactor segmentCompactor0 = segmentCompactorProvider.get();
+    SegmentCompactor segmentCompactor1 = segmentCompactorProvider.get();
+    assertNotEquals(segmentCompactor0, segmentCompactor1);
+  }
+
+}
