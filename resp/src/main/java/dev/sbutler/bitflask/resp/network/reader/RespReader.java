a6fdc8c0e0f9272895b625534afa89f9812b9ac4
Seth Butler <sbsoftware@protonmail.com>
2022-07-16 09:57:42 -0600

    resp: improve RespRead and RespWriter interface javadocs

--- resp/src/main/java/dev/sbutler/bitflask/resp/network/reader/RespReader.java
+++ resp/src/main/java/dev/sbutler/bitflask/resp/network/reader/RespReader.java
@@ -1,9 +1,19 @@
 package dev.sbutler.bitflask.resp.network.reader;
 
 import dev.sbutler.bitflask.resp.types.RespType;
+import java.io.EOFException;
 import java.io.IOException;
+import java.net.ProtocolException;
 
 public interface RespReader {
 
+  /**
+   * Reads the next RespType from the underlying input-stream
+   *
+   * @return the read RespType
+   * @throws EOFException      if the underlying input-stream is closed
+   * @throws ProtocolException if the read input data is malformed
+   * @throws IOException       if a general failure occurs while reading
+   */
   RespType<?> readNextRespType() throws IOException;
 }
--- resp/src/main/java/dev/sbutler/bitflask/resp/network/writer/RespWriter.java
+++ resp/src/main/java/dev/sbutler/bitflask/resp/network/writer/RespWriter.java
@@ -5,5 +5,11 @@ import java.io.IOException;
 
 public interface RespWriter {
 
+  /**
+   * Writes the provided RespType to the underlying output-stream
+   *
+   * @param respType the data to be written
+   * @throws IOException if a general failure occurs while reading
+   */
   void writeRespType(RespType<?> respType) throws IOException;
 }
