708ad0abf52bdbcc8f5fac1c0f82543aa726ccf8
Seth Butler <sbsoftware@protonmail.com>
2022-07-31 23:18:14 -0600

    storage: StorageConfigurationDefaultProvider inherit from ConfigurationDefaultProvider

--- storage/src/main/java/dev/sbutler/bitflask/storage/configuration/StorageConfigurationDefaultProvider.java
+++ storage/src/main/java/dev/sbutler/bitflask/storage/configuration/StorageConfigurationDefaultProvider.java
@@ -1,6 +1,6 @@
 package dev.sbutler.bitflask.storage.configuration;
 
-import com.beust.jcommander.IDefaultProvider;
+import dev.sbutler.bitflask.common.configuration.ConfigurationDefaultProvider;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ResourceBundle;
@@ -12,7 +12,7 @@ import java.util.ResourceBundle;
  * <p>If a ResourceBundle is provided, and it contains a value for a corresponding property, the
  * contained value will be provided. Otherwise, the hardcoded default value will be returned.
  */
-public class StorageConfigurationDefaultProvider implements IDefaultProvider {
+public class StorageConfigurationDefaultProvider extends ConfigurationDefaultProvider {
 
   static final String STORAGE_DISPATCHER_CAPACITY_PROPERTY_KEY = "storage.dispatcherCapacity";
   static final int DEFAULT_STORAGE_DISPATCHER_CAPACITY = 500;
@@ -24,50 +24,27 @@ public class StorageConfigurationDefaultProvider implements IDefaultProvider {
   static final String STORAGE_SEGMENT_SIZE_LIMIT_PROPERTY_KEY = "storage.segmentSizeLimit";
   static final long DEFAULT_STORAGE_SEGMENT_SIZE_LIMIT = 1048576L; // 1 MiB
 
-  private final ResourceBundle resourceBundle;
-
   public StorageConfigurationDefaultProvider() {
-    this.resourceBundle = null;
+    super();
   }
 
   public StorageConfigurationDefaultProvider(ResourceBundle resourceBundle) {
-    this.resourceBundle = resourceBundle;
+    super(resourceBundle);
   }
 
   @Override
   public String getDefaultValueFor(String optionName) {
     return switch (optionName) {
-      case StorageConfiguration.STORAGE_DISPATCHER_CAPACITY_FLAG -> getStorageDispatcherCapacity();
-      case StorageConfiguration.STORAGE_STORE_DIRECTORY_PATH_FLAG -> getStorageStoreDirectoryPath();
-      case StorageConfiguration.STORAGE_SEGMENT_SIZE_LIMIT_FLAG -> getStorageSegmentSizeLimit();
+      case StorageConfiguration.STORAGE_DISPATCHER_CAPACITY_FLAG ->
+          getWithPropertyKeyOrDefault(STORAGE_DISPATCHER_CAPACITY_PROPERTY_KEY,
+              DEFAULT_STORAGE_DISPATCHER_CAPACITY);
+      case StorageConfiguration.STORAGE_STORE_DIRECTORY_PATH_FLAG ->
+          getWithPropertyKeyOrDefault(STORAGE_STORE_DIRECTORY_PATH_PROPERTY_KEY,
+              DEFAULT_STORAGE_STORE_DIRECTORY_PATH);
+      case StorageConfiguration.STORAGE_SEGMENT_SIZE_LIMIT_FLAG ->
+          getWithPropertyKeyOrDefault(STORAGE_SEGMENT_SIZE_LIMIT_PROPERTY_KEY,
+              DEFAULT_STORAGE_SEGMENT_SIZE_LIMIT);
       default -> null;
     };
-  }
-
-  private String getStorageDispatcherCapacity() {
-    if (resourceBundle == null
-        || !resourceBundle.containsKey(STORAGE_DISPATCHER_CAPACITY_PROPERTY_KEY)) {
-      return String.valueOf(DEFAULT_STORAGE_DISPATCHER_CAPACITY);
-    } else {
-      return resourceBundle.getString(STORAGE_DISPATCHER_CAPACITY_PROPERTY_KEY);
-    }
-  }
-
-  private String getStorageStoreDirectoryPath() {
-    if (resourceBundle == null
-        || !resourceBundle.containsKey(STORAGE_STORE_DIRECTORY_PATH_PROPERTY_KEY)) {
-      return DEFAULT_STORAGE_STORE_DIRECTORY_PATH.toString();
-    } else {
-      return resourceBundle.getString(STORAGE_STORE_DIRECTORY_PATH_PROPERTY_KEY);
-    }
-  }
-
-  private String getStorageSegmentSizeLimit() {
-    if (resourceBundle == null
-        || !resourceBundle.containsKey(STORAGE_SEGMENT_SIZE_LIMIT_PROPERTY_KEY)) {
-      return String.valueOf(DEFAULT_STORAGE_SEGMENT_SIZE_LIMIT);
-    } else {
-      return resourceBundle.getString(STORAGE_SEGMENT_SIZE_LIMIT_PROPERTY_KEY);
-    }
   }
 }
