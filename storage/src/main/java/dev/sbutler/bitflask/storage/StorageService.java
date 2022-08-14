e9e6399f65c260fcc76690e569db12fc15dcd568
Seth Butler <sbsoftware@protonmail.com>
2022-08-14 17:31:23 -0600

    storage: use ServiceManager in StorageService

--- storage/src/main/java/dev/sbutler/bitflask/storage/StorageService.java
+++ storage/src/main/java/dev/sbutler/bitflask/storage/StorageService.java
@@ -2,9 +2,12 @@ package dev.sbutler.bitflask.storage;
 
 import static com.google.common.util.concurrent.MoreExecutors.shutdownAndAwaitTermination;
 
+import com.google.common.collect.ImmutableSet;
 import com.google.common.flogger.FluentLogger;
 import com.google.common.util.concurrent.AbstractExecutionThreadService;
 import com.google.common.util.concurrent.ListeningExecutorService;
+import com.google.common.util.concurrent.Service;
+import com.google.common.util.concurrent.ServiceManager;
 import dev.sbutler.bitflask.common.dispatcher.DispatcherSubmission;
 import dev.sbutler.bitflask.storage.commands.CommandMapper;
 import dev.sbutler.bitflask.storage.commands.StorageCommand;
@@ -15,6 +18,8 @@ import dev.sbutler.bitflask.storage.dispatcher.StorageResponse;
 import dev.sbutler.bitflask.storage.segment.SegmentManagerService;
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import javax.annotation.Nonnull;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
@@ -26,8 +31,8 @@ public final class StorageService extends AbstractExecutionThreadService {
 
   private static final FluentLogger logger = FluentLogger.forEnclosingClass();
 
+  private final ServiceManager serviceManager;
   private final ListeningExecutorService executorService;
-  private final SegmentManagerService segmentManagerService;
   private final StorageCommandDispatcher commandDispatcher;
   private final CommandMapper commandMapper;
   private volatile boolean isRunning = true;
@@ -37,14 +42,14 @@ public final class StorageService extends AbstractExecutionThreadService {
       SegmentManagerService segmentManagerService, StorageCommandDispatcher commandDispatcher,
       CommandMapper commandMapper) {
     this.executorService = executorService;
-    this.segmentManagerService = segmentManagerService;
     this.commandDispatcher = commandDispatcher;
     this.commandMapper = commandMapper;
+    this.serviceManager = new ServiceManager(ImmutableSet.of(segmentManagerService));
   }
 
   @Override
-  protected void startUp() throws Exception {
-    segmentManagerService.startUp();
+  protected void startUp() {
+    serviceManager.awaitHealthy();
     logger.atInfo().log("StorageService started");
   }
 
@@ -72,8 +77,36 @@ public final class StorageService extends AbstractExecutionThreadService {
     System.out.println("StorageService shutdown triggered");
     isRunning = false;
     commandDispatcher.closeAndDrain();
-    segmentManagerService.shutDown();
+    stopServices();
     shutdownAndAwaitTermination(executorService, Duration.ofSeconds(5));
     System.out.println("StorageService completed shutdown");
+  }
+
+  private void stopServices() {
+    // Give the services 5 seconds to stop to ensure that we are responsive to shut down
+    // requests.
+    try {
+      serviceManager.stopAsync().awaitStopped(5, TimeUnit.SECONDS);
+    } catch (TimeoutException timeout) {
+      // stopping timed out
+      System.err.println("StorageService's ServiceManager timed out while stopping" + timeout);
+    }
+  }
+
+  private void addServiceManagerListener() {
+    serviceManager.addListener(
+        new ServiceManager.Listener() {
+          public void stopped() {
+            System.out.println("StorageService: All services have stopped.");
+          }
+
+          public void healthy() {
+            logger.atInfo().log("All services have been initialized and are healthy");
+          }
+
+          public void failure(@Nonnull Service service) {
+            System.err.printf("StorageService: [%s] failed to start.", service.getClass());
+          }
+        }, executorService);
   }
 }
--- storage/src/test/java/dev/sbutler/bitflask/storage/StorageServiceTest.java
+++ storage/src/test/java/dev/sbutler/bitflask/storage/StorageServiceTest.java
@@ -7,10 +7,12 @@ import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockConstruction;
 import static org.mockito.Mockito.verify;
 
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.ListeningExecutorService;
+import com.google.common.util.concurrent.ServiceManager;
 import com.google.common.util.concurrent.SettableFuture;
 import com.google.common.util.concurrent.testing.TestingExecutors;
 import dev.sbutler.bitflask.common.dispatcher.DispatcherSubmission;
@@ -25,16 +27,15 @@ import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockedConstruction;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 class StorageServiceTest {
 
-  @InjectMocks
-  StorageService storage;
+  StorageService storageService;
   @Spy
   @SuppressWarnings("UnstableApiUsage")
   ListeningExecutorService executorService = TestingExecutors.sameThreadScheduledExecutor();
@@ -48,41 +49,51 @@ class StorageServiceTest {
   @SuppressWarnings("unchecked")
   @Test
   void processSubmission() throws Exception {
-    // Arrange
-    String key = "key", value = "value";
-    ReadDTO dto = new ReadDTO(key);
-    SettableFuture<StorageResponse> submissionResponseFuture = mock(SettableFuture.class);
-    DispatcherSubmission<StorageCommandDTO, StorageResponse> submission =
-        new DispatcherSubmission<>(dto, submissionResponseFuture);
-    doReturn(submission).when(storageCommandDispatcher).poll(anyLong(), any(TimeUnit.class));
-    StorageCommand command = mock(StorageCommand.class);
-    ListenableFuture<StorageResponse> commandResponseFuture = mock(ListenableFuture.class);
-    doReturn(commandResponseFuture).when(command).execute();
-    doReturn(command).when(commandMapper).mapToCommand(any(StorageCommandDTO.class));
-    // Act
-    activateStorageService();
-    // Assert
-    verify(commandMapper, atLeastOnce()).mapToCommand(dto);
-    verify(command, atLeastOnce()).execute();
-    verify(submissionResponseFuture, atLeastOnce()).setFuture(commandResponseFuture);
+    try (MockedConstruction<ServiceManager> serviceManagerMockedConstruction =
+        mockConstruction(ServiceManager.class)) {
+      storageService = new StorageService(executorService, segmentManagerService,
+          storageCommandDispatcher, commandMapper);
+      // Arrange
+      String key = "key", value = "value";
+      ReadDTO dto = new ReadDTO(key);
+      SettableFuture<StorageResponse> submissionResponseFuture = mock(SettableFuture.class);
+      DispatcherSubmission<StorageCommandDTO, StorageResponse> submission =
+          new DispatcherSubmission<>(dto, submissionResponseFuture);
+      doReturn(submission).when(storageCommandDispatcher).poll(anyLong(), any(TimeUnit.class));
+      StorageCommand command = mock(StorageCommand.class);
+      ListenableFuture<StorageResponse> commandResponseFuture = mock(ListenableFuture.class);
+      doReturn(commandResponseFuture).when(command).execute();
+      doReturn(command).when(commandMapper).mapToCommand(any(StorageCommandDTO.class));
+      // Act
+      storageService.startAsync().awaitRunning();
+      ServiceManager serviceManager = serviceManagerMockedConstruction.constructed().get(0);
+      doReturn(serviceManager).when(serviceManager).stopAsync();
+      Thread.sleep(100);
+      storageService.triggerShutdown();
+      storageService.awaitTerminated(Duration.ofSeconds(1));
+      // Assert
+      verify(commandMapper, atLeastOnce()).mapToCommand(dto);
+      verify(command, atLeastOnce()).execute();
+      verify(submissionResponseFuture, atLeastOnce()).setFuture(commandResponseFuture);
+    }
   }
 
   @Test
   void shutdown() throws Exception {
-    // Arrange
-    storage.startAsync().awaitRunning();
-    assertTrue(storage.isRunning());
-    // Act
-    storage.triggerShutdown();
-    storage.awaitTerminated(Duration.ofSeconds(1));
-    // Assert
-    assertFalse(storage.isRunning());
-  }
-
-  private void activateStorageService() throws Exception {
-    storage.startAsync().awaitRunning();
-    Thread.sleep(100);
-    storage.triggerShutdown();
-    storage.awaitTerminated(Duration.ofSeconds(1));
+    try (MockedConstruction<ServiceManager> serviceManagerMockedConstruction =
+        mockConstruction(ServiceManager.class)) {
+      // Arrange
+      storageService = new StorageService(executorService, segmentManagerService,
+          storageCommandDispatcher, commandMapper);
+      storageService.startAsync().awaitRunning();
+      assertTrue(storageService.isRunning());
+      ServiceManager serviceManager = serviceManagerMockedConstruction.constructed().get(0);
+      doReturn(serviceManager).when(serviceManager).stopAsync();
+      // Act
+      storageService.triggerShutdown();
+      storageService.awaitTerminated(Duration.ofSeconds(1));
+      // Assert
+      assertFalse(storageService.isRunning());
+    }
   }
 }
