c6c059dd5a053f4720e8cc37ef0038796c39557d
Seth Butler <sbsoftware@protonmail.com>
2022-10-01 17:27:13 -0600

    resp: delete RespReaderModule

--- resp/src/main/java/dev/sbutler/bitflask/resp/network/RespNetworkModule.java
+++ resp/src/main/java/dev/sbutler/bitflask/resp/network/RespNetworkModule.java
@@ -1,13 +1,21 @@
 package dev.sbutler.bitflask.resp.network;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class RespNetworkModule extends AbstractModule {
 
   @Override
   protected void configure() {
     super.configure();
-    install(new RespReaderModule());
   }
 
+  @Provides
+  @RespReaderBufferedReader
+  BufferedReader provideBufferedReader(InputStream inputStream) {
+    return new BufferedReader(new InputStreamReader(inputStream));
+  }
 }
--- resp/src/test/java/dev/sbutler/bitflask/resp/network/RespNetworkModuleTest.java
+++ resp/src/test/java/dev/sbutler/bitflask/resp/network/RespNetworkModuleTest.java
@@ -1,16 +1,42 @@
 package dev.sbutler.bitflask.resp.network;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockConstruction;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.Provides;
+import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.OutputStream;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedConstruction;
 
 public class RespNetworkModuleTest {
+
+  @Test
+  void configure() {
+    Injector injector = Guice.createInjector(
+        new MockStreamProvider(),
+        new RespNetworkModule()
+    );
+    injector.getProvider(RespReader.class);
+    injector.getProvider(RespWriter.class);
+  }
+
+  @Test
+  void provideBufferedReader() {
+    try (MockedConstruction<BufferedReader> bufferedReaderMockedConstruction = mockConstruction(
+        BufferedReader.class)) {
+      RespNetworkModule respNetworkModule = new RespNetworkModule();
+      InputStream inputStream = mock(InputStream.class);
+      BufferedReader providedBufferedReader = respNetworkModule.provideBufferedReader(inputStream);
+      BufferedReader mockedBufferedReader = bufferedReaderMockedConstruction.constructed().get(0);
+      assertEquals(mockedBufferedReader, providedBufferedReader);
+    }
+  }
 
   static class MockStreamProvider extends AbstractModule {
 
@@ -23,15 +49,5 @@ public class RespNetworkModuleTest {
     OutputStream provideOutputStream() {
       return mock(OutputStream.class);
     }
-  }
-
-  @Test
-  void configure() {
-    Injector injector = Guice.createInjector(
-        new MockStreamProvider(),
-        new RespNetworkModule()
-    );
-    injector.getProvider(RespReader.class);
-    injector.getProvider(RespWriter.class);
   }
 }
