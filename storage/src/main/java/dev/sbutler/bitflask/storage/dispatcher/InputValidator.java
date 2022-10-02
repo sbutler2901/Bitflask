84518e8a6b49551c2d5c7e623a04c138f2c15e9c
Seth Butler <sbsoftware@protonmail.com>
2022-10-02 13:24:14 -0600

    storage: command dto and unify input validation

---
+++ storage/src/main/java/dev/sbutler/bitflask/storage/dispatcher/InputValidator.java
@@ -0,0 +1,33 @@
+package dev.sbutler.bitflask.storage.dispatcher;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+
+class InputValidator {
+
+  static final int KEY_MAX_SIZE = 256;
+  static final int VALUE_MAX_SIZE = 1024;
+
+  private InputValidator() {
+
+  }
+
+  public static void validateKey(String key) {
+    checkNotNull(key);
+    checkArgument(!key.isBlank(), "Expected non-blank key, but was [%s]", key);
+    checkArgument(key.length() <= KEY_MAX_SIZE,
+        "Expect key smaller than [%d] characters, but was [%d]",
+        KEY_MAX_SIZE,
+        key.length());
+  }
+
+  public static void validateValue(String value) {
+    checkNotNull(value);
+    checkArgument(!value.isBlank(), "Expected non-blank value, but was [%s]", value);
+    checkArgument(value.length() <= VALUE_MAX_SIZE,
+        "Expect value smaller than [%d] characters, but was [%d]",
+        VALUE_MAX_SIZE,
+        value.length());
+  }
+
+}
--- storage/src/main/java/dev/sbutler/bitflask/storage/dispatcher/StorageCommandDTO.java
+++ storage/src/main/java/dev/sbutler/bitflask/storage/dispatcher/StorageCommandDTO.java
@@ -1,39 +1,36 @@
 package dev.sbutler.bitflask.storage.dispatcher;
 
-import static com.google.common.base.Preconditions.checkArgument;
-import static com.google.common.base.Preconditions.checkNotNull;
-
+/**
+ * The various commands that the storage engine can accept.
+ *
+ * <p>Input will be validated at construction, throwing an {@link NullPointerException} if a
+ * provided string was null, or {@link IllegalArgumentException} if the string is blank or too
+ * long.
+ *
+ * <p>A key or value must not be blank, defined by {@code string.isBlank()}. A key cannot be longer
+ * than 256, and a value no longer than 1024.
+ */
 public sealed interface StorageCommandDTO {
 
+  /**
+   * Use when the value mapped by the provided {@code key} should be read.
+   */
   record ReadDTO(String key) implements StorageCommandDTO {
 
     public ReadDTO {
-      validateKey(key);
-    }
-
-    private static void validateKey(String key) {
-      checkNotNull(key);
-      checkArgument(!key.isBlank(), "Expected non-blank key, but was [%s]", key);
-      checkArgument(key.length() <= 256, "Expect key smaller than 256 characters, but was [%d]",
-          key.length());
+      InputValidator.validateKey(key);
     }
   }
 
+  /**
+   * Use when the provided {@code key} should be written with a mapping to the provided
+   * {@code value}.
+   */
   record WriteDTO(String key, String value) implements StorageCommandDTO {
 
     public WriteDTO {
-      validateKey(key);
-      validateValue(value);
-    }
-
-    private static void validateKey(String key) {
-      checkArgument(!key.isBlank(), "Expected non-blank key, but was [%s]", key);
-      checkArgument(key.length() <= 256, "Expect key smaller than 256 characters, but was [%d]",
-          key.length());
-    }
-
-    private static void validateValue(String value) {
-
+      InputValidator.validateKey(key);
+      InputValidator.validateValue(value);
     }
   }
 }
