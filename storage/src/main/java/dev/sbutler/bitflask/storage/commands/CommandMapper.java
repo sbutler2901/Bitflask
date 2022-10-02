3250e95358fc4d0e13580685dd73fecab3964105
Seth Butler <sbsoftware@protonmail.com>
2022-10-02 13:42:02 -0600

    storage: update CommandMapper for deletion dto and command

--- storage/src/main/java/dev/sbutler/bitflask/storage/commands/CommandMapper.java
+++ storage/src/main/java/dev/sbutler/bitflask/storage/commands/CommandMapper.java
@@ -3,12 +3,16 @@ package dev.sbutler.bitflask.storage.commands;
 import com.google.common.util.concurrent.ListeningExecutorService;
 import dev.sbutler.bitflask.storage.configuration.concurrency.StorageExecutorService;
 import dev.sbutler.bitflask.storage.dispatcher.StorageCommandDTO;
+import dev.sbutler.bitflask.storage.dispatcher.StorageCommandDTO.DeleteDTO;
 import dev.sbutler.bitflask.storage.dispatcher.StorageCommandDTO.ReadDTO;
 import dev.sbutler.bitflask.storage.dispatcher.StorageCommandDTO.WriteDTO;
 import dev.sbutler.bitflask.storage.segment.SegmentManagerService;
 import dev.sbutler.bitflask.storage.segment.SegmentManagerService.ManagedSegments;
 import javax.inject.Inject;
 
+/**
+ * Maps incoming {@link StorageCommandDTO}s into executable {@link StorageCommand}s.
+ */
 public class CommandMapper {
 
   private final ListeningExecutorService executorService;
@@ -26,6 +30,7 @@ public class CommandMapper {
     return switch (commandDTO) {
       case ReadDTO readDTO -> new ReadCommand(executorService, managedSegments, readDTO);
       case WriteDTO writeDTO -> new WriteCommand(executorService, managedSegments, writeDTO);
+      case DeleteDTO deleteDTO -> new DeleteCommand(executorService, managedSegments, deleteDTO);
     };
   }
 }
