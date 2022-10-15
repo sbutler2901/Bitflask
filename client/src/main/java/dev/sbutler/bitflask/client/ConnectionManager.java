ed71e1a05ee1aadf95ac6847216e80bd28cd4b4d
Seth Butler <sbsoftware@protonmail.com>
2022-10-14 23:40:33 -0600

    client: reduce ConnectionManager visibility

--- client/src/main/java/dev/sbutler/bitflask/client/ConnectionManager.java
+++ client/src/main/java/dev/sbutler/bitflask/client/ConnectionManager.java
@@ -8,7 +8,7 @@ import javax.inject.Inject;
 import javax.inject.Singleton;
 
 @Singleton
-public class ConnectionManager implements AutoCloseable {
+class ConnectionManager implements AutoCloseable {
 
   private final Socket socket;
 
