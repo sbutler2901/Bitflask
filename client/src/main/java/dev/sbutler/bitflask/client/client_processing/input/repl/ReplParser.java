b6b1e4a86563ccd0afe02b630607069f12cb8ddb
Seth Butler <sbsoftware@protonmail.com>
2022-10-25 21:30:56 -0600

    client: create ReplParser to provide interface for the ReplReader

---
+++ client/src/main/java/dev/sbutler/bitflask/client/client_processing/input/repl/ReplParser.java
@@ -0,0 +1,23 @@
+package dev.sbutler.bitflask.client.client_processing.input.repl;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableList.Builder;
+import dev.sbutler.bitflask.client.client_processing.input.repl.types.ReplElement;
+import java.io.IOException;
+
+public final class ReplParser {
+
+  public static ImmutableList<ReplElement> readNextLine(ReplReader reader) {
+    try {
+      ImmutableList.Builder<ReplElement> input = new Builder<>();
+      input.add(reader.readString());
+      input.addAll(reader.readToEndLine());
+      return input.build();
+    } catch (IOException e) {
+      throw new ReplIOException("Failed to read next line", e);
+    }
+  }
+
+  private ReplParser() {
+  }
+}
