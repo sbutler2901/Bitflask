f20fe0dd0f5012af532278924885c1615cf1c25e
Seth Butler <sbsoftware@protonmail.com>
2022-10-25 23:33:20 -0600

    client: implement quoted string parsing

--- client/src/main/java/dev/sbutler/bitflask/client/client_processing/input/repl/ReplReader.java
+++ client/src/main/java/dev/sbutler/bitflask/client/client_processing/input/repl/ReplReader.java
@@ -10,12 +10,24 @@ import dev.sbutler.bitflask.client.client_processing.input.repl.types.ReplSingle
 import dev.sbutler.bitflask.client.client_processing.input.repl.types.ReplString;
 import java.io.IOException;
 import java.io.Reader;
+import java.util.function.Supplier;
 
 public final class ReplReader implements AutoCloseable {
 
+  /**
+   * Various special chars used by the Repl parser for easy reuse.
+   */
+  private static class SpecialChars {
+
+    static final String SINGLE_QUOTE = "'";
+    static final String DOUBLE_QUOTE = "\"";
+    static final String BACK_SLASH = "\\";
+    static final String NEW_LINE = "\n";
+  }
+
   private final Reader reader;
 
-  private int peeked;
+  private String peeked;
   private ReplToken peekedAsToken = null;
 
   public ReplReader(Reader reader) {
@@ -26,15 +38,16 @@ public final class ReplReader implements AutoCloseable {
     if (peekedAsToken == ReplToken.END_DOCUMENT) {
       return;
     }
-    peeked = reader.read();
-    peekedAsToken = mapToToken(peeked);
+    int read = reader.read();
+    peekedAsToken = mapToToken(read);
+    peeked = Character.toString(read);
   }
 
   ReplString readString() throws IOException {
     StringBuilder builder = new StringBuilder();
-    readAllWhiteSpace();
-    while (!isPeekedSpace() || peekedIsNotEnd()) {
-      builder.append(Character.toString(peeked));
+    readAllWhiteSpaceBeforeNextElement();
+    while (shouldContinueParsingElement()) {
+      builder.append(peeked);
       peek();
     }
     return new ReplString(builder.toString());
@@ -42,45 +55,51 @@ public final class ReplReader implements AutoCloseable {
 
   @SuppressWarnings("UnstableApiUsage")
   ReplInteger readInteger() throws IOException {
+    ReplElement readElement = attemptReadingInteger();
+    if (readElement instanceof ReplString replString) {
+      throw new ReplSyntaxException(
+          String.format("A Repl Integer could not be read: [%s]", replString.getAsString()));
+    }
+    return (ReplInteger) readElement;
+  }
+
+  /**
+   * Attempts to read the next element as a ReplInteger, returning a ReplString of the value read if
+   * not.
+   */
+  @SuppressWarnings("UnstableApiUsage")
+  private ReplElement attemptReadingInteger() throws IOException {
+    // Try to read as number
     ReplString replString = readString();
     Long parsed = Longs.tryParse(replString.getAsString());
     if (parsed == null) {
-      throw new ReplSyntaxException(
-          String.format("A Repl Integer could not be read: [%s]", replString.getAsString()));
+      // was not a number
+      return replString;
     }
     return new ReplInteger(parsed);
   }
 
   ReplSingleQuotedString readReplSingleQuotedString() throws IOException {
-    readAllWhiteSpace();
-    return new ReplSingleQuotedString("");
+    readAllWhiteSpaceBeforeNextElement();
+    String quotedString = parseQuotedString();
+    return new ReplSingleQuotedString(quotedString);
   }
 
   ReplDoubleQuotedString readReplDoubleQuotedString() throws IOException {
-    readAllWhiteSpace();
-    return new ReplDoubleQuotedString("");
+    readAllWhiteSpaceBeforeNextElement();
+    String quotedString = parseQuotedString();
+    return new ReplDoubleQuotedString(quotedString);
   }
 
-  @SuppressWarnings("UnstableApiUsage")
   ImmutableList<ReplElement> readToEndLine() throws IOException {
     ImmutableList.Builder<ReplElement> builder = new Builder<>();
-    readAllWhiteSpace();
     while (peekedIsNotEnd()) {
       ReplElement element =
           switch (peekedAsToken) {
             case SINGLE_QUOTE -> readReplSingleQuotedString();
             case DOUBLE_QUOTE -> readReplDoubleQuotedString();
             case CHARACTER -> readString();
-            case NUMBER -> {
-              // Try to read as number
-              ReplString replString = readString();
-              Long parsed = Longs.tryParse(replString.getAsString());
-              if (parsed == null) {
-                // was not a number
-                yield replString;
-              }
-              yield new ReplInteger(parsed);
-            }
+            case NUMBER -> attemptReadingInteger();
             default -> throw new IllegalStateException("Invalid token found: " + peekedAsToken);
           };
       builder.add(element);
@@ -88,10 +107,68 @@ public final class ReplReader implements AutoCloseable {
     return builder.build();
   }
 
-  private void readAllWhiteSpace() throws IOException {
+  private String parseQuotedString() throws IOException {
+    ReplToken startQuote = peekedAsToken;
+    Supplier<String> escapeHandler = startQuote == ReplToken.DOUBLE_QUOTE
+        ? this::doubleQuoteEscapeHandler
+        : this::singleQuoteEscapeHandler;
+
+    peek(); // consume start quote
+
+    StringBuilder builder = new StringBuilder();
+    boolean escapeActive = false;
+    while (shouldContinueParsingElement()) {
+      if (escapeActive) {
+        String result = escapeHandler.get();
+        builder.append(result);
+        escapeActive = false;
+      } else if (peekedAsToken == startQuote) {
+        // quote complete
+        break;
+      } else if (isPeekedBackSlash()) {
+        // start escape for next char
+        escapeActive = true;
+      } else {
+        builder.append(peeked);
+      }
+    }
+    if (peekedAsToken != startQuote) {
+      throw new ReplSyntaxException("Quoted element was not properly terminated");
+    }
+    return builder.toString();
+  }
+
+  private String doubleQuoteEscapeHandler() {
+    if (isPeekedBackSlash() || isPeekedDoubleQuote()) {
+      return peeked;
+    }
+    if (peeked.equals("n")) {
+      return SpecialChars.NEW_LINE;
+    }
+    // Unsupported escape, include backslash
+    return SpecialChars.BACK_SLASH + peeked;
+  }
+
+  private String singleQuoteEscapeHandler() {
+    if (isPeekedBackSlash() || isPeekedSingleQuote()) {
+      return peeked;
+    }
+    // Unsupported escape, include backslash
+    return SpecialChars.BACK_SLASH + peeked;
+  }
+
+  private void readAllWhiteSpaceBeforeNextElement() throws IOException {
+    // Ensure there is at least a single whitespace
+    if (!isPeekedSpace() && peekedIsNotEnd()) {
+      throw new ReplSyntaxException("Whitespace expected before parsing next element");
+    }
     while (isPeekedSpace() && peekedIsNotEnd()) {
       peek();
     }
+  }
+
+  private boolean shouldContinueParsingElement() {
+    return !isPeekedSpace() || peekedIsNotEnd();
   }
 
   private boolean peekedIsNotEnd() {
@@ -145,10 +222,10 @@ public final class ReplReader implements AutoCloseable {
     }
     String asString = Character.toString(read);
     return switch (asString) {
-      case "'" -> ReplToken.SINGLE_QUOTE;
-      case "\"" -> ReplToken.DOUBLE_QUOTE;
-      case "\\" -> ReplToken.BACK_SLASH;
-      case "\n" -> ReplToken.END_LINE;
+      case SpecialChars.SINGLE_QUOTE -> ReplToken.SINGLE_QUOTE;
+      case SpecialChars.DOUBLE_QUOTE -> ReplToken.DOUBLE_QUOTE;
+      case SpecialChars.BACK_SLASH -> ReplToken.BACK_SLASH;
+      case SpecialChars.NEW_LINE -> ReplToken.END_LINE;
       default -> throw new ReplSyntaxException(
           String.format("Could not map to ReplToken: int [%d], string [%s]", read, asString));
     };
