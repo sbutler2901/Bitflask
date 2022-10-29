fe7d37de5754dbe2b639aefafa6a30e7c09c61ad
Seth Butler <sbsoftware@protonmail.com>
2022-10-28 21:44:36 -0600

    client: import InputToArgsConverterTest tests to ReplReaderTest

--- client/src/test/java/dev/sbutler/bitflask/client/client_processing/input/repl/ReplReaderTest.java
+++ client/src/test/java/dev/sbutler/bitflask/client/client_processing/input/repl/ReplReaderTest.java
@@ -1,9 +1,12 @@
 package dev.sbutler.bitflask.client.client_processing.input.repl;
 
 import static com.google.common.truth.Truth.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.google.common.collect.ImmutableList;
 import dev.sbutler.bitflask.client.client_processing.input.repl.types.ReplElement;
+import dev.sbutler.bitflask.client.client_processing.input.repl.types.ReplString;
 import java.io.Reader;
 import java.io.StringReader;
 import org.junit.jupiter.api.Test;
@@ -20,7 +23,365 @@ public class ReplReaderTest {
     ImmutableList<ReplElement> elements = replReader.readToEndLine();
     // Assert
     assertThat(elements).hasSize(3);
-    assertThat(elements).containsExactly("set", "test", "value").inOrder();
+    ImmutableList<ReplElement> expected = ImmutableList.of(
+        new ReplString("set"),
+        new ReplString("test"),
+        new ReplString("value")
+    );
+    assertThat(elements).containsExactlyElementsIn(expected).inOrder();
   }
 
+  @Test
+  void inlineSingleQuote() throws Exception {
+    // Arrange
+    String value = "set test val'ue";
+    Reader reader = new StringReader(value);
+    ReplReader replReader = new ReplReader(reader);
+    // Act
+    ImmutableList<ReplElement> elements = replReader.readToEndLine();
+    // Assert
+    assertThat(elements).hasSize(3);
+    ImmutableList<ReplElement> expected = ImmutableList.of(
+        new ReplString("set"),
+        new ReplString("test"),
+        new ReplString("val'ue")
+    );
+    assertThat(elements).containsExactlyElementsIn(expected).inOrder();
+  }
+
+  @Test
+  void inlineDoubleQuote() throws Exception {
+    // Arrange
+    String value = "set test val\"ue";
+    Reader reader = new StringReader(value);
+    ReplReader replReader = new ReplReader(reader);
+    // Act
+    ImmutableList<ReplElement> elements = replReader.readToEndLine();
+    // Assert
+    assertThat(elements).hasSize(3);
+    ImmutableList<ReplElement> expected = ImmutableList.of(
+        new ReplString("set"),
+        new ReplString("test"),
+        new ReplString("val\"ue")
+    );
+    assertThat(elements).containsExactlyElementsIn(expected).inOrder();
+  }
+
+  @Test
+  void singleQuote_withoutBreaks() throws Exception {
+    // Arrange
+    String value = "set test 'value'";
+    Reader reader = new StringReader(value);
+    ReplReader replReader = new ReplReader(reader);
+    // Act
+    ImmutableList<ReplElement> elements = replReader.readToEndLine();
+    // Assert
+    assertThat(elements).hasSize(3);
+    ImmutableList<ReplElement> expected = ImmutableList.of(
+        new ReplString("set"),
+        new ReplString("test"),
+        new ReplString("value")
+    );
+    assertThat(elements).containsExactlyElementsIn(expected).inOrder();
+  }
+
+  @Test
+  void doubleQuote_withoutBreaks() throws Exception {
+    // Arrange
+    String value = "set test \"value\"";
+    Reader reader = new StringReader(value);
+    ReplReader replReader = new ReplReader(reader);
+    // Act
+    ImmutableList<ReplElement> elements = replReader.readToEndLine();
+    // Assert
+    assertThat(elements).hasSize(3);
+    ImmutableList<ReplElement> expected = ImmutableList.of(
+        new ReplString("set"),
+        new ReplString("test"),
+        new ReplString("value")
+    );
+    assertThat(elements).containsExactlyElementsIn(expected).inOrder();
+  }
+
+  @Test
+  void singleQuote_withSpaces() throws Exception {
+    // Arrange
+    String value = "set test 'value other'";
+    Reader reader = new StringReader(value);
+    ReplReader replReader = new ReplReader(reader);
+    // Act
+    ImmutableList<ReplElement> elements = replReader.readToEndLine();
+    // Assert
+    assertThat(elements).hasSize(3);
+    ImmutableList<ReplElement> expected = ImmutableList.of(
+        new ReplString("set"),
+        new ReplString("test"),
+        new ReplString("value other")
+    );
+    assertThat(elements).containsExactlyElementsIn(expected).inOrder();
+  }
+
+  @Test
+  void doubleQuote_withSpaces() throws Exception {
+    // Arrange
+    String value = "set test \"value other\"";
+    Reader reader = new StringReader(value);
+    ReplReader replReader = new ReplReader(reader);
+    // Act
+    ImmutableList<ReplElement> elements = replReader.readToEndLine();
+    // Assert
+    assertThat(elements).hasSize(3);
+    ImmutableList<ReplElement> expected = ImmutableList.of(
+        new ReplString("set"),
+        new ReplString("test"),
+        new ReplString("value other")
+    );
+    assertThat(elements).containsExactlyElementsIn(expected).inOrder();
+  }
+
+  @Test
+  void singleQuote_spaceAfter() throws Exception {
+    // Arrange
+    String value = "'set' 'test' 'value'";
+    Reader reader = new StringReader(value);
+    ReplReader replReader = new ReplReader(reader);
+    // Act
+    ImmutableList<ReplElement> elements = replReader.readToEndLine();
+    // Assert
+    assertThat(elements).hasSize(3);
+    ImmutableList<ReplElement> expected = ImmutableList.of(
+        new ReplString("set"),
+        new ReplString("test"),
+        new ReplString("value")
+    );
+    assertThat(elements).containsExactlyElementsIn(expected).inOrder();
+  }
+
+  @Test
+  void doubleQuote_spaceAfter() throws Exception {
+    // Arrange
+    String value = "\"set\" \"test\" \"value\"";
+    Reader reader = new StringReader(value);
+    ReplReader replReader = new ReplReader(reader);
+    // Act
+    ImmutableList<ReplElement> elements = replReader.readToEndLine();
+    // Assert
+    assertThat(elements).hasSize(3);
+    ImmutableList<ReplElement> expected = ImmutableList.of(
+        new ReplString("set"),
+        new ReplString("test"),
+        new ReplString("value")
+    );
+    assertThat(elements).containsExactlyElementsIn(expected).inOrder();
+  }
+
+  @Test
+  void singleQuote_startOfInput() throws Exception {
+    // Arrange
+    String value = "'test'";
+    Reader reader = new StringReader(value);
+    ReplReader replReader = new ReplReader(reader);
+    // Act
+    ImmutableList<ReplElement> elements = replReader.readToEndLine();
+    // Assert
+    assertThat(elements).hasSize(3);
+    ImmutableList<ReplElement> expected = ImmutableList.of(
+        new ReplString("test")
+    );
+    assertThat(elements).containsExactlyElementsIn(expected).inOrder();
+  }
+
+  @Test
+  void doubleQuote_startOfInput() throws Exception {
+    // Arrange
+    String value = "\"test\"";
+    Reader reader = new StringReader(value);
+    ReplReader replReader = new ReplReader(reader);
+    // Act
+    ImmutableList<ReplElement> elements = replReader.readToEndLine();
+    // Assert
+    assertThat(elements).hasSize(3);
+    ImmutableList<ReplElement> expected = ImmutableList.of(
+        new ReplString("test")
+    );
+    assertThat(elements).containsExactlyElementsIn(expected).inOrder();
+  }
+
+  @Test
+  void singleQuote_withEscape_singleQuote() throws Exception {
+    // Arrange
+    String value = "set test 'value \\'other'";
+    Reader reader = new StringReader(value);
+    ReplReader replReader = new ReplReader(reader);
+    // Act
+    ImmutableList<ReplElement> elements = replReader.readToEndLine();
+    // Assert
+    assertThat(elements).hasSize(3);
+    ImmutableList<ReplElement> expected = ImmutableList.of(
+        new ReplString("set"),
+        new ReplString("test"),
+        new ReplString("value 'other")
+    );
+    assertThat(elements).containsExactlyElementsIn(expected).inOrder();
+  }
+
+  @Test
+  void doubleQuote_withEscape_doubleQuote() throws Exception {
+    // Arrange
+    String value = "set test \"value \\\"other\"";
+    Reader reader = new StringReader(value);
+    ReplReader replReader = new ReplReader(reader);
+    // Act
+    ImmutableList<ReplElement> elements = replReader.readToEndLine();
+    // Assert
+    assertThat(elements).hasSize(3);
+    ImmutableList<ReplElement> expected = ImmutableList.of(
+        new ReplString("set"),
+        new ReplString("test"),
+        new ReplString("value \"other")
+    );
+    assertThat(elements).containsExactlyElementsIn(expected).inOrder();
+  }
+
+  @Test
+  void singleQuote_withEscape_backslash() throws Exception {
+    // Arrange
+    String value = "set test 'value \\\\other'";
+    Reader reader = new StringReader(value);
+    ReplReader replReader = new ReplReader(reader);
+    // Act
+    ImmutableList<ReplElement> elements = replReader.readToEndLine();
+    // Assert
+    assertThat(elements).hasSize(3);
+    ImmutableList<ReplElement> expected = ImmutableList.of(
+        new ReplString("set"),
+        new ReplString("test"),
+        new ReplString("value \"other")
+    );
+    assertThat(elements).containsExactlyElementsIn(expected).inOrder();
+  }
+
+  @Test
+  void doubleQuote_withEscape_backslash() throws Exception {
+    // Arrange
+    String value = "set test \"value \\\\other\"";
+    Reader reader = new StringReader(value);
+    ReplReader replReader = new ReplReader(reader);
+    // Act
+    ImmutableList<ReplElement> elements = replReader.readToEndLine();
+    // Assert
+    assertThat(elements).hasSize(3);
+    ImmutableList<ReplElement> expected = ImmutableList.of(
+        new ReplString("set"),
+        new ReplString("test"),
+        new ReplString("value \\other")
+    );
+    assertThat(elements).containsExactlyElementsIn(expected).inOrder();
+  }
+
+  @Test
+  void singleQuote_unsupportedEscape() throws Exception {
+    // Arrange
+    String value = "set test 'value\\nother'";
+    Reader reader = new StringReader(value);
+    ReplReader replReader = new ReplReader(reader);
+    // Act
+    ImmutableList<ReplElement> elements = replReader.readToEndLine();
+    // Assert
+    assertThat(elements).hasSize(3);
+    ImmutableList<ReplElement> expected = ImmutableList.of(
+        new ReplString("set"),
+        new ReplString("test"),
+        new ReplString("value\\nother")
+    );
+    assertThat(elements).containsExactlyElementsIn(expected).inOrder();
+  }
+
+  @Test
+  void doubleQuote_unsupportedEscape() throws Exception {
+    // Arrange
+    String value = "set test \"value\\rother\"";
+    Reader reader = new StringReader(value);
+    ReplReader replReader = new ReplReader(reader);
+    // Act
+    ImmutableList<ReplElement> elements = replReader.readToEndLine();
+    // Assert
+    assertThat(elements).hasSize(3);
+    ImmutableList<ReplElement> expected = ImmutableList.of(
+        new ReplString("set"),
+        new ReplString("test"),
+        new ReplString("value\\rother")
+    );
+    assertThat(elements).containsExactlyElementsIn(expected).inOrder();
+  }
+
+  @Test
+  void singleQuote_unterminated_endOfInput() throws Exception {
+    // Arrange
+    String value = "set test 'value\"";
+    Reader reader = new StringReader(value);
+    ReplReader replReader = new ReplReader(reader);
+    // Act
+    ReplSyntaxException exception =
+        assertThrows(ReplSyntaxException.class, replReader::readToEndLine);
+    // Assert
+    assertTrue(exception.getMessage().toLowerCase().contains("not properly terminated"));
+  }
+
+  @Test
+  void doubleQuote_unterminated_endOfInput() throws Exception {
+    // Arrange
+    String value = "set test \"value'";
+    Reader reader = new StringReader(value);
+    ReplReader replReader = new ReplReader(reader);
+    // Act
+    ReplSyntaxException exception =
+        assertThrows(ReplSyntaxException.class, replReader::readToEndLine);
+    // Assert
+    assertTrue(exception.getMessage().toLowerCase().contains("not properly terminated"));
+  }
+
+  @Test
+  void singleQuote_unterminated_spaceNotFollowing() throws Exception {
+    // Arrange
+    String value = "set test 'value'a";
+    Reader reader = new StringReader(value);
+    ReplReader replReader = new ReplReader(reader);
+    // Act
+    ReplSyntaxException exception =
+        assertThrows(ReplSyntaxException.class, replReader::readToEndLine);
+    // Assert
+    assertTrue(exception.getMessage().toLowerCase().contains("not properly terminated"));
+  }
+
+  @Test
+  void doubleQuote_unterminated_spaceNotFollowing() throws Exception {
+    // Arrange
+    String value = "set test \"value\"a";
+    Reader reader = new StringReader(value);
+    ReplReader replReader = new ReplReader(reader);
+    // Act
+    ReplSyntaxException exception =
+        assertThrows(ReplSyntaxException.class, replReader::readToEndLine);
+    // Assert
+    assertTrue(exception.getMessage().toLowerCase().contains("not properly terminated"));
+  }
+
+  @Test
+  void doubleQuote_withEscape_newline() throws Exception {
+    // Arrange
+    String value = "set test \"value\\nother\"";
+    Reader reader = new StringReader(value);
+    ReplReader replReader = new ReplReader(reader);
+    // Act
+    ImmutableList<ReplElement> elements = replReader.readToEndLine();
+    // Assert
+    assertThat(elements).hasSize(3);
+    ImmutableList<ReplElement> expected = ImmutableList.of(
+        new ReplString("set"),
+        new ReplString("test"),
+        new ReplString("value\nother")
+    );
+    assertThat(elements).containsExactlyElementsIn(expected).inOrder();
+  }
 }
