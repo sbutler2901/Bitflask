0c5166db43b2c32795cf1b1986d251043dc52b67
Seth Butler <sbsoftware@protonmail.com>
2023-01-12 22:40:30 -0700

    server: improve NetworkService test coverage

--- server/src/test/java/dev/sbutler/bitflask/server/network_service/NetworkServiceTest.java
+++ server/src/test/java/dev/sbutler/bitflask/server/network_service/NetworkServiceTest.java
@@ -2,6 +2,7 @@ package dev.sbutler.bitflask.server.network_service;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -9,11 +10,13 @@ import static org.mockito.Mockito.when;
 
 import com.google.common.util.concurrent.ListeningExecutorService;
 import com.google.common.util.concurrent.testing.TestingExecutors;
+import dev.sbutler.bitflask.server.network_service.NetworkService.Factory;
+import java.io.IOException;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -21,8 +24,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 @ExtendWith(MockitoExtension.class)
 public class NetworkServiceTest {
 
-  @InjectMocks
   private NetworkService networkService;
+
   @Spy
   @SuppressWarnings("UnstableApiUsage")
   private ListeningExecutorService executorService = TestingExecutors.sameThreadScheduledExecutor();
@@ -30,6 +33,12 @@ public class NetworkServiceTest {
   private ServerSocketChannel serverSocketChannel;
   @Mock
   private ClientHandlingService.Factory clientHandlingServiceFactory;
+
+  @BeforeEach
+  void beforeEach() {
+    NetworkService.Factory factory = new Factory(executorService, clientHandlingServiceFactory);
+    networkService = factory.create(serverSocketChannel);
+  }
 
   @Test
   void run() throws Exception {
@@ -52,5 +61,13 @@ public class NetworkServiceTest {
     verify(clientHandlingService, times(1)).addListener(any(), any());
     verify(serverSocketChannel, times(1)).close();
     verify(clientHandlingService, times(1)).stopAsync();
+  }
+
+  @Test
+  void triggerShutdown_close_throwsIOException() throws Exception {
+    // Arrange
+    doThrow(IOException.class).when(serverSocketChannel).close();
+    // Act
+    networkService.triggerShutdown();
   }
 }
