aba18eed471974738b9c19b2e8c4c179c32d794c
Seth Butler <sbsoftware@protonmail.com>
2023-02-28 00:41:51 -0700

    storage: implement SegmentIndexEntry and tests

--- storage/src/main/java/dev/sbutler/bitflask/storage/segment/SegmentIndexEntry.java
+++ storage/src/main/java/dev/sbutler/bitflask/storage/segment/SegmentIndexEntry.java
@@ -1,14 +1,56 @@
 package dev.sbutler.bitflask.storage.segment;
 
+
+import static com.google.common.base.Preconditions.checkArgument;
+
+import com.google.common.primitives.Bytes;
+import com.google.common.primitives.Longs;
+import dev.sbutler.bitflask.common.primitives.UnsignedShort;
+import dev.sbutler.bitflask.storage.entry.Entry;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+/**
+ * A single key:offset entry in a segment index.
+ *
+ * <p>The key cannot be empty and not greater than {@link Entry#KEY_MAX_LENGTH}. The offset cannot
+ * be negative.
+ */
 record SegmentIndexEntry(String key, long offset) {
 
+  SegmentIndexEntry {
+    checkArgument(!key.isEmpty(), "Key must not be empty.");
+    checkArgument(key.length() <= Entry.KEY_MAX_LENGTH,
+        "Key length greater than allowed. Provided [%s], max allowed [%s]",
+        key.length(),
+        Entry.KEY_MAX_LENGTH);
+    checkArgument(offset >= 0L, "Offset negative. Provided [%s]", offset);
+  }
+
+  /**
+   * creates a new SegmentIndexEntry from the provided byte array.
+   *
+   * <p>The first 2 indices will be interpreted as a 2-byte unsigned short representing the key
+   * length. The next 8 indices will be interpreted as an 8-byte long representing the offset. The
+   * derived key length will determine the amount of bytes for the key.
+   */
   static SegmentIndexEntry fromBytes(byte[] bytes) {
-    // TODO: implement
-    return null;
+    byte[] keyLengthBytes = Arrays.copyOfRange(bytes, 0, UnsignedShort.BYTES);
+
+    int offsetEnd = Long.BYTES + UnsignedShort.BYTES;
+    byte[] offsetBytes = Arrays.copyOfRange(bytes, UnsignedShort.BYTES, offsetEnd);
+
+    UnsignedShort keyLength = UnsignedShort.fromBytes(keyLengthBytes);
+    byte[] keyBytes = Arrays.copyOfRange(bytes, offsetEnd, offsetEnd + keyLength.value());
+
+    return new SegmentIndexEntry(new String(keyBytes), Longs.fromByteArray(offsetBytes));
   }
 
   byte[] getBytes() {
-    // TODO: implement
-    return new byte[0];
+    byte[] keyLengthBytes = UnsignedShort.valueOf(key.length()).getBytes();
+    byte[] offsetBytes = Longs.toByteArray(offset);
+    byte[] keyBytes = key.getBytes(StandardCharsets.UTF_8);
+
+    return Bytes.concat(keyLengthBytes, offsetBytes, keyBytes);
   }
 }
--- storage/src/test/java/dev/sbutler/bitflask/storage/segment/SegmentIndexEntryTest.java
+++ storage/src/test/java/dev/sbutler/bitflask/storage/segment/SegmentIndexEntryTest.java
@@ -1,5 +1,87 @@
 package dev.sbutler.bitflask.storage.segment;
 
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.assertThrows;
+
+import com.google.common.primitives.Bytes;
+import com.google.common.primitives.Longs;
+import dev.sbutler.bitflask.common.primitives.UnsignedShort;
+import java.nio.charset.StandardCharsets;
+import org.junit.jupiter.api.Test;
+
 public class SegmentIndexEntryTest {
 
+  @Test
+  public void fromBytes_valid() {
+    String key = "key";
+    long offset = 0L;
+    byte[] bytes = Bytes.concat(
+        UnsignedShort.valueOf(key.length()).getBytes(),
+        Longs.toByteArray(offset),
+        key.getBytes(StandardCharsets.UTF_8));
+
+    SegmentIndexEntry entry = SegmentIndexEntry.fromBytes(bytes);
+
+    assertThat(entry.key()).isEqualTo(key);
+    assertThat(entry.offset()).isEqualTo(offset);
+  }
+
+  @Test
+  public void fromBytes_invalid_keyLength_zero_throwsIllegalArgumentException() {
+    long offset = 0L;
+    byte[] bytes = Bytes.concat(
+        UnsignedShort.valueOf(0).getBytes(),
+        Longs.toByteArray(offset),
+        "".getBytes(StandardCharsets.UTF_8));
+
+    IllegalArgumentException e =
+        assertThrows(IllegalArgumentException.class, () -> SegmentIndexEntry.fromBytes(bytes));
+
+    assertThat(e).hasMessageThat().ignoringCase().contains("Key must not be empty.");
+  }
+
+  @Test
+  public void fromBytes_invalid_keyLength_tooLarge_throwsIllegalArgumentException() {
+    String key = "key";
+    long offset = 0L;
+    byte[] bytes = Bytes.concat(
+        new byte[UnsignedShort.BYTES + 1],
+        Longs.toByteArray(offset),
+        key.getBytes(StandardCharsets.UTF_8));
+
+    IllegalArgumentException e =
+        assertThrows(IllegalArgumentException.class, () -> SegmentIndexEntry.fromBytes(bytes));
+
+    assertThat(e).hasMessageThat().ignoringCase().contains("Key must not be empty.");
+  }
+
+  @Test
+  public void fromBytes_invalid_offset_negative_throwsIllegalArgumentException() {
+    String key = "key";
+    long offset = -1L;
+    byte[] bytes = Bytes.concat(
+        UnsignedShort.valueOf(key.length()).getBytes(),
+        Longs.toByteArray(offset),
+        key.getBytes(StandardCharsets.UTF_8));
+
+    IllegalArgumentException e =
+        assertThrows(IllegalArgumentException.class, () -> SegmentIndexEntry.fromBytes(bytes));
+
+    assertThat(e).hasMessageThat().ignoringCase().contains("Offset negative.");
+  }
+
+  @Test
+  public void getBytes_valid() {
+    String key = "key";
+    long offset = 0L;
+    SegmentIndexEntry entry = new SegmentIndexEntry(key, offset);
+    byte[] expectedBytes = Bytes.concat(
+        UnsignedShort.valueOf(key.length()).getBytes(),
+        Longs.toByteArray(offset),
+        key.getBytes(StandardCharsets.UTF_8));
+
+    byte[] bytes = entry.getBytes();
+
+    assertThat(bytes).isEqualTo(expectedBytes);
+  }
 }
