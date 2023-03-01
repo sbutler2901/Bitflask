3f8315c29db6531d32560c271bd41e7bfb633032
Seth Butler <sbsoftware@protonmail.com>
2023-02-28 20:16:50 -0700

    storage: extract segment and segmentIndex creation into separate functions

--- storage/src/main/java/dev/sbutler/bitflask/storage/segment/SegmentFactory.java
+++ storage/src/main/java/dev/sbutler/bitflask/storage/segment/SegmentFactory.java
@@ -69,58 +69,74 @@ public final class SegmentFactory {
   }
 
   /**
-   * Creates a new Segment a segment level 0.
-   *
-   * <p>This includes creating the segment file and its associated index file.
+   * Creates a new Segment at segment level 0 and its associated index file.
    */
   public ListenableFuture<Segment> create(ImmutableSortedMap<String, Entry> keyEntryMap) {
-    return Futures.submit(() -> build(keyEntryMap), executorService);
+    return Futures.submit(() -> {
+      UnsignedShort segmentNumber = UnsignedShort.valueOf(nextSegmentNumber.getAndIncrement());
+
+      SegmentIndex segmentIndex = createSegmentIndex(keyEntryMap, segmentNumber);
+      return createSegment(keyEntryMap, segmentNumber, segmentIndex);
+    }, executorService);
   }
 
-  private Segment build(ImmutableSortedMap<String, Entry> keyEntryMap) throws IOException {
-    UnsignedShort segmentNumber = UnsignedShort.valueOf(nextSegmentNumber.getAndIncrement());
-    SegmentIndexMetadata indexMetadata = new SegmentIndexMetadata(segmentNumber);
+  /**
+   * Creates a new {@link Segment} at segment level 0.
+   */
+  Segment createSegment(ImmutableSortedMap<String, Entry> keyEntryMap,
+      UnsignedShort segmentNumber, SegmentIndex segmentIndex) throws IOException {
+
     SegmentMetadata segmentMetadata = new SegmentMetadata(
         segmentNumber, UnsignedShort.valueOf(0));
-
-    ImmutableSortedMap.Builder<String, Long> indexKeyOffsetMap = ImmutableSortedMap.naturalOrder();
     BloomFilter<String> keyFilter = BloomFilter.create(Funnels.stringFunnel(
         StandardCharsets.UTF_8), keyEntryMap.size());
 
-    String storeDirectoryFilePath = configurations.getStorageStoreDirectoryPath().toString();
-    Path segmentPath = Path.of(storeDirectoryFilePath,
+    Path segmentPath = Path.of(configurations.getStorageStoreDirectoryPath().toString(),
         Segment.createFileName(segmentNumber.value()));
-    Path indexPath = Path.of(storeDirectoryFilePath,
-        SegmentIndex.createFileName(segmentNumber.value()));
 
     try (BufferedOutputStream segmentOutputStream = new BufferedOutputStream(
-        Files.newOutputStream(segmentPath, StandardOpenOption.CREATE_NEW));
-        BufferedOutputStream indexOutputStream =
-            new BufferedOutputStream(
-                Files.newOutputStream(indexPath, StandardOpenOption.CREATE_NEW))) {
+        Files.newOutputStream(segmentPath, StandardOpenOption.CREATE_NEW))) {
 
       byte[] segmentMetadataBytes = segmentMetadata.getBytes();
       segmentOutputStream.write(segmentMetadataBytes);
-      indexOutputStream.write(indexMetadata.getBytes());
 
-      long segmentOffset = segmentMetadataBytes.length;
       for (Entry entry : keyEntryMap.values()) {
-        SegmentIndexEntry indexEntry = new SegmentIndexEntry(entry.key(), segmentOffset);
-        indexOutputStream.write(indexEntry.getBytes());
-        indexKeyOffsetMap.put(entry.key(), segmentOffset);
-
-        byte[] entryBytes = entry.getBytes();
         segmentOutputStream.write(entry.getBytes());
-        segmentOffset += entryBytes.length;
 
         keyFilter.put(entry.key());
       }
     }
 
     EntryReader entryReader = entryReaderFactory.create(segmentPath);
-    SegmentIndex segmentIndex = new SegmentIndexDense(indexMetadata,
-        indexKeyOffsetMap.build());
 
     return Segment.create(segmentMetadata, entryReader, keyFilter, segmentIndex);
+  }
+
+  /**
+   * Creates a new {@link SegmentIndex}.
+   */
+  SegmentIndex createSegmentIndex(ImmutableSortedMap<String, Entry> keyEntryMap,
+      UnsignedShort segmentNumber) throws IOException {
+    SegmentIndexMetadata indexMetadata = new SegmentIndexMetadata(segmentNumber);
+    ImmutableSortedMap.Builder<String, Long> indexKeyOffsetMap = ImmutableSortedMap.naturalOrder();
+
+    Path indexPath = Path.of(configurations.getStorageStoreDirectoryPath().toString(),
+        SegmentIndex.createFileName(segmentNumber.value()));
+
+    try (BufferedOutputStream indexOutputStream = new BufferedOutputStream(
+        Files.newOutputStream(indexPath, StandardOpenOption.CREATE_NEW))) {
+
+      indexOutputStream.write(indexMetadata.getBytes());
+
+      long segmentOffset = SegmentMetadata.BYTES;
+      for (Entry entry : keyEntryMap.values()) {
+        SegmentIndexEntry indexEntry = new SegmentIndexEntry(entry.key(), segmentOffset);
+        indexOutputStream.write(indexEntry.getBytes());
+        indexKeyOffsetMap.put(entry.key(), segmentOffset);
+        segmentOffset += entry.getBytes().length;
+      }
+    }
+
+    return new SegmentIndexDense(indexMetadata, indexKeyOffsetMap.build());
   }
 }
