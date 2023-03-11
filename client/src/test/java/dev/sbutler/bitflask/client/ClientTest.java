3c37834b888c628976bc126b787fba8a33b1b61b
Seth Butler <sbsoftware@protonmail.com>
2023-03-10 19:36:44 -0700

    client: delete unused test files. Fix Client tests
    (cherry picked from commit 85cc470e296f77b9adc022b0136b7658c4d8c398)

--- client/src/test/java/dev/sbutler/bitflask/client/ClientTest.java
+++ client/src/test/java/dev/sbutler/bitflask/client/ClientTest.java
@@ -16,102 +16,124 @@ import com.google.inject.ProvisionException;
 import dev.sbutler.bitflask.client.client_processing.ReplClientProcessorService;
 import dev.sbutler.bitflask.resp.network.RespService;
 import java.io.IOException;
+import java.net.SocketAddress;
+import java.nio.channels.SocketChannel;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
 
 public class ClientTest {
 
-  private final Injector injector = mock(Injector.class);
-  private final ReplClientProcessorService replProcessor =
+  private final Injector INJECTOR = mock(Injector.class);
+  private final ReplClientProcessorService REPL_PROCESSOR =
       mock(ReplClientProcessorService.class);
-  private final RespService respService =
+  private final RespService RESP_SERVICE =
       mock(RespService.class);
+  private final SocketChannel SOCKET_CHANNEL = mock(SocketChannel.class);
 
   @Test
   void success() {
-    try (MockedStatic<Guice> guiceMockedStatic = mockStatic(Guice.class)) {
+    try (MockedStatic<SocketChannel> socketChannelMockedStatic = mockStatic(SocketChannel.class);
+        MockedStatic<RespService> respServiceMockedStatic = mockStatic(RespService.class);
+        MockedStatic<Guice> guiceMockedStatic = mockStatic(Guice.class)) {
       // Arrange
-      when(injector.getInstance(ReplClientProcessorService.class))
-          .thenReturn(replProcessor);
-      when(injector.getInstance(RespService.class))
-          .thenReturn(respService);
+      socketChannelMockedStatic.when(() -> SocketChannel.open(any(SocketAddress.class)))
+          .thenReturn(SOCKET_CHANNEL);
+      respServiceMockedStatic.when(() -> RespService.create(any())).thenReturn(RESP_SERVICE);
       guiceMockedStatic.when(() -> Guice.createInjector(any(ClientModule.class)))
-          .thenReturn(injector);
+          .thenReturn(INJECTOR);
+      when(INJECTOR.getInstance(ReplClientProcessorService.class))
+          .thenReturn(REPL_PROCESSOR);
       // Act
       Client.main(new String[0]);
       // Assert
-      verify(replProcessor, times(1)).run();
+      verify(REPL_PROCESSOR, times(1)).run();
     }
   }
 
   @Test
   void getInstance_throwsProvisionException() {
-    try (MockedStatic<Guice> guiceMockedStatic = mockStatic(Guice.class)) {
+    try (MockedStatic<SocketChannel> socketChannelMockedStatic = mockStatic(SocketChannel.class);
+        MockedStatic<RespService> respServiceMockedStatic = mockStatic(RespService.class);
+        MockedStatic<Guice> guiceMockedStatic = mockStatic(Guice.class)) {
       // Arrange
-      when(injector.getInstance(ReplClientProcessorService.class))
-          .thenThrow(new ProvisionException("test"));
+      socketChannelMockedStatic.when(() -> SocketChannel.open(any(SocketAddress.class)))
+          .thenReturn(SOCKET_CHANNEL);
+      respServiceMockedStatic.when(() -> RespService.create(any())).thenReturn(RESP_SERVICE);
       guiceMockedStatic.when(() -> Guice.createInjector(any(ClientModule.class)))
-          .thenReturn(injector);
+          .thenReturn(INJECTOR);
+      when(INJECTOR.getInstance(ReplClientProcessorService.class))
+          .thenThrow(new ProvisionException("test"));
       // Act
       Client.main(new String[0]);
       // Assert
-      verify(replProcessor, times(0)).run();
+      verify(REPL_PROCESSOR, times(0)).run();
     }
   }
 
   @Test
   void getInstance_throwsConfigurationException() {
-    try (MockedStatic<Guice> guiceMockedStatic = mockStatic(Guice.class)) {
+    try (MockedStatic<SocketChannel> socketChannelMockedStatic = mockStatic(SocketChannel.class);
+        MockedStatic<RespService> respServiceMockedStatic = mockStatic(RespService.class);
+        MockedStatic<Guice> guiceMockedStatic = mockStatic(Guice.class)) {
       // Arrange
-      when(injector.getInstance(ReplClientProcessorService.class))
-          .thenThrow(new ConfigurationException(ImmutableList.of()));
+      socketChannelMockedStatic.when(() -> SocketChannel.open(any(SocketAddress.class)))
+          .thenReturn(SOCKET_CHANNEL);
+      respServiceMockedStatic.when(() -> RespService.create(any())).thenReturn(RESP_SERVICE);
       guiceMockedStatic.when(() -> Guice.createInjector(any(ClientModule.class)))
-          .thenReturn(injector);
+          .thenReturn(INJECTOR);
+      when(INJECTOR.getInstance(ReplClientProcessorService.class))
+          .thenThrow(new ConfigurationException(ImmutableList.of()));
 
       // Act
       Client.main(new String[0]);
       // Assert
-      verify(replProcessor, times(0)).run();
+      verify(REPL_PROCESSOR, times(0)).run();
     }
   }
 
   @Test
   void unexpectedFailure() {
-    try (MockedStatic<Guice> guiceMockedStatic = mockStatic(Guice.class)) {
+    try (MockedStatic<SocketChannel> socketChannelMockedStatic = mockStatic(SocketChannel.class);
+        MockedStatic<RespService> respServiceMockedStatic = mockStatic(RespService.class);
+        MockedStatic<Guice> guiceMockedStatic = mockStatic(Guice.class)) {
       // Arrange
-      when(injector.getInstance(ReplClientProcessorService.class))
-          .thenReturn(replProcessor);
-      when(injector.getInstance(RespService.class))
-          .thenReturn(respService);
+      socketChannelMockedStatic.when(() -> SocketChannel.open(any(SocketAddress.class)))
+          .thenReturn(SOCKET_CHANNEL);
+      respServiceMockedStatic.when(() -> RespService.create(any())).thenReturn(RESP_SERVICE);
       guiceMockedStatic.when(() -> Guice.createInjector(any(ClientModule.class)))
-          .thenReturn(injector);
+          .thenReturn(INJECTOR);
+      when(INJECTOR.getInstance(ReplClientProcessorService.class))
+          .thenReturn(REPL_PROCESSOR);
 
-      doThrow(new RuntimeException("test")).when(replProcessor).run();
+      doThrow(new RuntimeException("test")).when(REPL_PROCESSOR).run();
 
       // Act
       Client.main(new String[0]);
       // Assert
-      verify(replProcessor, times(1)).run();
+      verify(REPL_PROCESSOR, times(1)).run();
     }
   }
 
   @Test
   void closeConnection_throwsIoException() throws Exception {
-    try (MockedStatic<Guice> guiceMockedStatic = mockStatic(Guice.class)) {
+    try (MockedStatic<SocketChannel> socketChannelMockedStatic = mockStatic(SocketChannel.class);
+        MockedStatic<RespService> respServiceMockedStatic = mockStatic(RespService.class);
+        MockedStatic<Guice> guiceMockedStatic = mockStatic(Guice.class)) {
       // Arrange
-      when(injector.getInstance(ReplClientProcessorService.class))
-          .thenReturn(replProcessor);
-      when(injector.getInstance(RespService.class))
-          .thenReturn(respService);
+      socketChannelMockedStatic.when(() -> SocketChannel.open(any(SocketAddress.class)))
+          .thenReturn(SOCKET_CHANNEL);
+      respServiceMockedStatic.when(() -> RespService.create(any())).thenReturn(RESP_SERVICE);
       guiceMockedStatic.when(() -> Guice.createInjector(any(ClientModule.class)))
-          .thenReturn(injector);
+          .thenReturn(INJECTOR);
+      when(INJECTOR.getInstance(ReplClientProcessorService.class))
+          .thenReturn(REPL_PROCESSOR);
 
-      doThrow(new IOException("test")).when(respService).close();
+      doThrow(new IOException("test")).when(RESP_SERVICE).close();
 
       // Act
       Client.main(new String[0]);
       // Assert
-      verify(replProcessor, times(1)).run();
+      verify(REPL_PROCESSOR, times(1)).run();
     }
   }
 }
