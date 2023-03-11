406b88653e89a56b76454d615d4255044acdfb76
Seth Butler <sbsoftware@protonmail.com>
2023-03-11 12:26:40 -0700

    storage: stub MemtableLoader logic

--- storage/src/main/java/dev/sbutler/bitflask/storage/lsm/memtable/MemtableLoader.java
+++ storage/src/main/java/dev/sbutler/bitflask/storage/lsm/memtable/MemtableLoader.java
@@ -1,25 +1,35 @@
 package dev.sbutler.bitflask.storage.lsm.memtable;
 
+import dev.sbutler.bitflask.storage.configuration.StorageConfigurations;
 import dev.sbutler.bitflask.storage.exceptions.StorageException;
 import java.io.IOException;
+import java.nio.file.Path;
 import javax.inject.Inject;
 
 public final class MemtableLoader {
 
-  private final WriteAheadLog.Factory writeAheadLogFactory;
+  private final StorageConfigurations configurations;
 
   @Inject
-  MemtableLoader(WriteAheadLog.Factory writeAheadLogFactory) {
-    this.writeAheadLogFactory = writeAheadLogFactory;
+  MemtableLoader(StorageConfigurations configurations) {
+    this.configurations = configurations;
   }
 
   public Memtable load() {
     // TODO: implement proper loading from disk
     try {
-      WriteAheadLog writeAheadLog = writeAheadLogFactory.create();
+      // if truncate, load WAL w/o entries and create Memtable,
+      // otherwise, load WAL entries, and create Memtable
+      WriteAheadLog writeAheadLog = WriteAheadLog.createFromPreExisting(getWriteAheadLogPath());
       return Memtable.create(writeAheadLog);
     } catch (IOException e) {
       throw new StorageException(e);
     }
+  }
+
+  private Path getWriteAheadLogPath() {
+    return Path.of(
+        configurations.getStorageStoreDirectoryPath().toString(),
+        WriteAheadLog.FILE_NAME + WriteAheadLog.FILE_EXTENSION);
   }
 }
--- storage/src/main/java/dev/sbutler/bitflask/storage/lsm/memtable/WriteAheadLog.java
+++ storage/src/main/java/dev/sbutler/bitflask/storage/lsm/memtable/WriteAheadLog.java
@@ -1,13 +1,11 @@
 package dev.sbutler.bitflask.storage.lsm.memtable;
 
-import dev.sbutler.bitflask.storage.configuration.StorageConfigurations;
 import dev.sbutler.bitflask.storage.lsm.entry.Entry;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
-import javax.inject.Inject;
 
 /**
  * A write-ahead-log for entries stored in the {@link Memtable}.
@@ -19,7 +17,7 @@ import javax.inject.Inject;
  * <p>It is expected that there is only a single WriteAheadLog file at a time which corresponds to
  * the current in memory Memtable.
  */
-public final class WriteAheadLog implements AutoCloseable {
+final class WriteAheadLog implements AutoCloseable {
 
   /**
    * The filename of WriteHeadLog files.
@@ -39,48 +37,29 @@ public final class WriteAheadLog implements AutoCloseable {
   }
 
   /**
-   * A factory for creating {@link WriteAheadLog instances}.
+   * Creates a new {@link WriteAheadLog} truncating any pre-existing WriteAheadLog file.
    */
-  public static class Factory {
+  public static WriteAheadLog create(Path path) throws IOException {
+    return new WriteAheadLog(
+        Files.newOutputStream(
+            path,
+            new StandardOpenOption[]{
+                StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.CREATE,
+                StandardOpenOption.WRITE, StandardOpenOption.DSYNC}));
+  }
 
-    private final StorageConfigurations configurations;
-
-    @Inject
-    Factory(StorageConfigurations configurations) {
-      this.configurations = configurations;
-    }
-
-    /**
-     * Creates a new {@link WriteAheadLog} truncating any pre-existing WriteAheadLog file.
-     */
-    public WriteAheadLog create() throws IOException {
-      return new WriteAheadLog(
-          Files.newOutputStream(
-              getPath(),
-              new StandardOpenOption[]{
-                  StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.CREATE,
-                  StandardOpenOption.WRITE, StandardOpenOption.DSYNC}));
-    }
-
-    /**
-     * Creates a {@link WriteAheadLog} from a pre-existing file with new writes appending to it.
-     *
-     * <p>A file will be created if one does not already exist.
-     */
-    public WriteAheadLog createFromPreExisting() throws IOException {
-      return new WriteAheadLog(
-          Files.newOutputStream(
-              getPath(),
-              new StandardOpenOption[]{
-                  StandardOpenOption.APPEND, StandardOpenOption.CREATE,
-                  StandardOpenOption.WRITE, StandardOpenOption.DSYNC}));
-    }
-
-    private Path getPath() {
-      return Path.of(
-          configurations.getStorageStoreDirectoryPath().toString(),
-          FILE_NAME + FILE_EXTENSION);
-    }
+  /**
+   * Creates a {@link WriteAheadLog} from a pre-existing file with new writes appending to it.
+   *
+   * <p>A file will be created if one does not already exist.
+   */
+  public static WriteAheadLog createFromPreExisting(Path path) throws IOException {
+    return new WriteAheadLog(
+        Files.newOutputStream(
+            path,
+            new StandardOpenOption[]{
+                StandardOpenOption.APPEND, StandardOpenOption.CREATE,
+                StandardOpenOption.WRITE, StandardOpenOption.DSYNC}));
   }
 
   public void append(Entry entry) throws IOException {
