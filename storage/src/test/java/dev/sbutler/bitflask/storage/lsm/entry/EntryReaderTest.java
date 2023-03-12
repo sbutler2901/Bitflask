8871ad4960ee8c050dcff8236340c60459893992
Seth Butler <sbsoftware@protonmail.com>
2023-03-11 19:37:04 -0700

    storage: add tests for EntryReader.readAllEntriesFromOffset()

--- storage/src/test/java/dev/sbutler/bitflask/storage/lsm/entry/EntryReaderTest.java
+++ storage/src/test/java/dev/sbutler/bitflask/storage/lsm/entry/EntryReaderTest.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mockStatic;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.primitives.Bytes;
 import dev.sbutler.bitflask.common.primitives.UnsignedShort;
 import dev.sbutler.bitflask.storage.lsm.segment.Segment;
@@ -35,6 +36,90 @@ public class EntryReaderTest {
   private final EntryReader entryReader = EntryReader.create(FILE_PATH);
 
   @Test
+  public void readAllEntriesFromOffset() throws Exception {
+    InputStream is = new ByteArrayInputStream(Bytes.concat(
+        ENTRY_0.getBytes(),
+        ENTRY_1.getBytes()));
+
+    try (MockedStatic<Files> filesMockedStatic = mockStatic(Files.class)) {
+      filesMockedStatic.when(() -> Files.newInputStream(any(), any())).thenReturn(is);
+      ImmutableList<Entry> entries = entryReader.readAllEntriesFromOffset(0L);
+      assertThat(entries).containsExactly(ENTRY_0, ENTRY_1);
+    }
+  }
+
+  @Test
+  public void readAllEntriesFromOffset_skipToOffset() throws Exception {
+    InputStream is = new ByteArrayInputStream(Bytes.concat(
+        ENTRY_0.getBytes(),
+        ENTRY_1.getBytes()));
+
+    try (MockedStatic<Files> filesMockedStatic = mockStatic(Files.class)) {
+      filesMockedStatic.when(() -> Files.newInputStream(any(), any())).thenReturn(is);
+      ImmutableList<Entry> entries =
+          entryReader.readAllEntriesFromOffset(ENTRY_0.getBytes().length);
+      assertThat(entries).containsExactly(ENTRY_1);
+    }
+  }
+
+  @Test
+  public void readAllEntriesFromOffset_numberReadMismatch_key() {
+    String key = "key";
+    String value = "value";
+
+    EntryMetadata storedMetadata = new EntryMetadata(
+        Instant.now().getEpochSecond(),
+        UnsignedShort.valueOf(key.length()),
+        UnsignedShort.valueOf(value.length()));
+    InputStream is = new ByteArrayInputStream(storedMetadata.getBytes());
+
+    try (MockedStatic<Files> filesMockedStatic = mockStatic(Files.class)) {
+      filesMockedStatic.when(() -> Files.newInputStream(any(), any())).thenReturn(is);
+      IOException e =
+          assertThrows(IOException.class,
+              () -> entryReader.readAllEntriesFromOffset(0L));
+
+      assertThat(e).hasMessageThat().ignoringCase()
+          .contains("Read key length did not match entry.");
+    }
+  }
+
+  @Test
+  public void readAllEntriesFromOffset_numberReadMismatch_value() {
+    String key = "key";
+    String value = "value";
+
+    EntryMetadata storedMetadata = new EntryMetadata(
+        Instant.now().getEpochSecond(),
+        UnsignedShort.valueOf(key.length()),
+        UnsignedShort.valueOf(value.length()));
+    InputStream is = new ByteArrayInputStream(Bytes.concat(
+        storedMetadata.getBytes(),
+        key.getBytes(StandardCharsets.UTF_8)));
+
+    try (MockedStatic<Files> filesMockedStatic = mockStatic(Files.class)) {
+      filesMockedStatic.when(() -> Files.newInputStream(any(), any())).thenReturn(is);
+      IOException e =
+          assertThrows(IOException.class,
+              () -> entryReader.readAllEntriesFromOffset(0L));
+
+      assertThat(e).hasMessageThat().ignoringCase()
+          .contains("Read value length did not match entry.");
+    }
+  }
+
+  @Test
+  public void readAllEntriesFromOffset_emptyFile() throws Exception {
+    InputStream is = new ByteArrayInputStream(new byte[0]);
+
+    try (MockedStatic<Files> filesMockedStatic = mockStatic(Files.class)) {
+      filesMockedStatic.when(() -> Files.newInputStream(any(), any())).thenReturn(is);
+      ImmutableList<Entry> entries = entryReader.readAllEntriesFromOffset(0L);
+      assertThat(entries).isEmpty();
+    }
+  }
+
+  @Test
   public void findEntryFromOffset_found() throws Exception {
     InputStream is = new ByteArrayInputStream(ENTRY_0.getBytes());
 
@@ -53,8 +138,7 @@ public class EntryReaderTest {
 
     try (MockedStatic<Files> filesMockedStatic = mockStatic(Files.class)) {
       filesMockedStatic.when(() -> Files.newInputStream(any(), any())).thenReturn(is);
-      Optional<Entry> entry = entryReader
-          .findEntryFromOffset(ENTRY_1.key(), ENTRY_0.getBytes().length);
+      Optional<Entry> entry = entryReader.findEntryFromOffset(ENTRY_1.key(), 0L);
       assertThat(entry).hasValue(ENTRY_1);
     }
   }
