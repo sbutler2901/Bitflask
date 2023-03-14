28b1572e8db5afc0fde95be3c5d5f241aa8e4b88
Seth Butler <sbsoftware@protonmail.com>
2023-03-13 18:24:50 -0600

    storage: implement tests for SegmentLoader

--- storage/src/test/java/dev/sbutler/bitflask/storage/lsm/segment/SegmentLoaderTest.java
+++ storage/src/test/java/dev/sbutler/bitflask/storage/lsm/segment/SegmentLoaderTest.java
@@ -8,6 +8,7 @@ import static org.mockito.Mockito.mockStatic;
 import static org.mockito.Mockito.when;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 import dev.sbutler.bitflask.storage.configuration.StorageConfigurations;
 import dev.sbutler.bitflask.storage.exceptions.StorageLoadException;
 import dev.sbutler.bitflask.storage.lsm.utils.LoaderUtils;
@@ -17,47 +18,55 @@ import java.util.concurrent.ThreadFactory;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
 
-public class SegmentIndexLoaderTest {
-
-  private static final Path PATH_0 = Path.of("/tmp/index_0.idx");
-  private static final Path PATH_1 = Path.of("/tmp/index_1.idx");
+public class SegmentLoaderTest {
 
   private final SegmentIndex index_0 = mock(SegmentIndex.class);
   private final SegmentIndex index_1 = mock(SegmentIndex.class);
 
+  private static final Path PATH_0 = Path.of("/tmp/segment_0.seg");
+  private static final Path PATH_1 = Path.of("/tmp/segment_1.seg");
+
+  private final Segment segment_0 = mock(Segment.class);
+  private final Segment segment_1 = mock(Segment.class);
+
+  private final ImmutableMap<Integer, SegmentIndex> segmentNumberToIndexMap =
+      ImmutableMap.of(0, index_0, 1, index_1);
+
   private final ThreadFactory threadFactory = Thread.ofVirtual().factory();
   private final StorageConfigurations configurations = mock(StorageConfigurations.class);
-  private final SegmentIndexFactory segmentIndexFactory = mock(SegmentIndexFactory.class);
+  private final SegmentFactory segmentFactory = mock(SegmentFactory.class);
 
-  private final SegmentIndexLoader loader =
-      new SegmentIndexLoader(threadFactory, configurations, segmentIndexFactory);
+  private final SegmentLoader loader =
+      new SegmentLoader(threadFactory, configurations, segmentFactory);
 
   @Test
-  public void load_success() throws Exception {
+  public void loadWithIndexes_success() throws Exception {
     try (MockedStatic<LoaderUtils> loaderUtilsMockedStatic = mockStatic(LoaderUtils.class)) {
       loaderUtilsMockedStatic.when(() -> LoaderUtils.loadPathsInDirForGlob(any(), any()))
           .thenReturn(ImmutableList.of(PATH_0, PATH_1));
-      when(segmentIndexFactory.loadFromPath(PATH_0)).thenReturn(index_0);
-      when(segmentIndexFactory.loadFromPath(PATH_1)).thenReturn(index_1);
+      when(segmentFactory.loadFromPath(PATH_0, segmentNumberToIndexMap)).thenReturn(segment_0);
+      when(segmentFactory.loadFromPath(PATH_1, segmentNumberToIndexMap)).thenReturn(segment_1);
 
-      ImmutableList<SegmentIndex> indexes = loader.load();
+      ImmutableList<Segment> segments = loader.loadWithIndexes(segmentNumberToIndexMap);
 
-      assertThat(indexes).containsExactly(index_0, index_1);
+      assertThat(segments).containsExactly(segment_0, segment_1);
     }
   }
 
   @Test
-  public void load_indexFactoryThrowsIoException_throwsStorageLoadException() throws Exception {
+  public void loadWithIndexes_indexFactoryThrowsIoException_throwsStorageLoadException()
+      throws Exception {
     try (MockedStatic<LoaderUtils> loaderUtilsMockedStatic = mockStatic(LoaderUtils.class)) {
       loaderUtilsMockedStatic.when(() -> LoaderUtils.loadPathsInDirForGlob(any(), any()))
           .thenReturn(ImmutableList.of(PATH_0));
       IOException ioException = new IOException("test");
-      when(segmentIndexFactory.loadFromPath(PATH_0)).thenThrow(ioException);
+      when(segmentFactory.loadFromPath(PATH_0, segmentNumberToIndexMap)).thenThrow(ioException);
 
-      StorageLoadException e = assertThrows(StorageLoadException.class, loader::load);
+      StorageLoadException e = assertThrows(StorageLoadException.class,
+          () -> loader.loadWithIndexes(segmentNumberToIndexMap));
 
       assertThat(e).hasCauseThat().isEqualTo(ioException);
-      assertThat(e).hasMessageThat().isEqualTo("Failed loading SegmentIndexes");
+      assertThat(e).hasMessageThat().isEqualTo("Failed loading Segments");
     }
   }
 }
