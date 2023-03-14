8feb7058f7ab2d07bb7240f457a1979bffd7c89b
Seth Butler <sbsoftware@protonmail.com>
2023-03-13 19:36:01 -0600

    storage: refactor static fields in SegmentIndexFactoryTest

--- storage/src/test/java/dev/sbutler/bitflask/storage/lsm/segment/SegmentIndexFactoryTest.java
+++ storage/src/test/java/dev/sbutler/bitflask/storage/lsm/segment/SegmentIndexFactoryTest.java
@@ -23,45 +23,58 @@ import org.mockito.MockedStatic;
 
 public class SegmentIndexFactoryTest {
 
-  private final Path TEST_RESOURCE_PATH = Paths.get("src/test/resources/");
-  private final StorageConfigurations CONFIG = mock(StorageConfigurations.class);
+  private static final Path INDEX_PATH = Path.of("/tmp/index_0.idx");
+  private static final Path TEST_RESOURCE_PATH = Paths.get("src/test/resources/");
 
-  private final Entry ENTRY_0 = new Entry(Instant.now().getEpochSecond(), "key0", "value0");
-  private final Entry ENTRY_1 = new Entry(Instant.now().getEpochSecond(), "key1", "value1");
+  private static final Entry ENTRY_0 =
+      new Entry(Instant.now().getEpochSecond(), "key0", "value0");
+  private static final Entry ENTRY_1 =
+      new Entry(Instant.now().getEpochSecond(), "key1", "value1");
 
-  private final UnsignedShort SEGMENT_NUMBER = UnsignedShort.valueOf(0);
+  private static final long ENTRY_0_OFFSET = SegmentMetadata.BYTES;
+  private static final long ENTRY_1_OFFSET = SegmentMetadata.BYTES + ENTRY_0.getBytes().length;
+  private static final ImmutableSortedMap<String, Long> KEY_OFFSET_MAP =
+      ImmutableSortedMap.<String, Long>naturalOrder()
+          .put(ENTRY_0.key(), ENTRY_0_OFFSET)
+          .put(ENTRY_1.key(), ENTRY_1_OFFSET)
+          .build();
+
+  private static final UnsignedShort SEGMENT_NUMBER = UnsignedShort.valueOf(0);
+
+  private static final SegmentIndexMetadata METADATA = new SegmentIndexMetadata(SEGMENT_NUMBER);
+  private static final SegmentIndexEntry INDEX_ENTRY_0 =
+      new SegmentIndexEntry(ENTRY_0.key(), ENTRY_0_OFFSET);
+  private static final SegmentIndexEntry INDEX_ENTRY_1 =
+      new SegmentIndexEntry(ENTRY_1.key(), ENTRY_1_OFFSET);
+
+  private final StorageConfigurations config = mock(StorageConfigurations.class);
 
   private SegmentIndexFactory indexFactory;
 
   @BeforeEach
   public void beforeEach() {
-    when(CONFIG.getStorageStoreDirectoryPath()).thenReturn(TEST_RESOURCE_PATH);
-    indexFactory = new SegmentIndexFactory(CONFIG);
+    when(config.getStorageStoreDirectoryPath()).thenReturn(TEST_RESOURCE_PATH);
+    indexFactory = new SegmentIndexFactory(config);
   }
 
   @Test
   public void createSegmentIndex() throws Exception {
-    long entryOffset0 = SegmentMetadata.BYTES;
-    long entryOffset1 = entryOffset0 + ENTRY_0.getBytes().length;
-    ImmutableSortedMap<String, Long> keyOffsetMap = ImmutableSortedMap.<String, Long>naturalOrder()
-        .put(ENTRY_0.key(), entryOffset0).put(ENTRY_1.key(), entryOffset1).build();
-
     SegmentIndex segmentIndex;
 
     ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
     try (MockedStatic<Files> fileMockedStatic = mockStatic(Files.class)) {
       fileMockedStatic.when(() -> Files.newOutputStream(any(), any())).thenReturn(outputStream);
 
-      segmentIndex = indexFactory.create(keyOffsetMap, SEGMENT_NUMBER);
+      segmentIndex = indexFactory.create(KEY_OFFSET_MAP, SEGMENT_NUMBER);
     }
 
     assertThat(outputStream.toByteArray()).isEqualTo(Bytes.concat(
-        new SegmentIndexMetadata(SEGMENT_NUMBER).getBytes(),
-        new SegmentIndexEntry(ENTRY_0.key(), entryOffset0).getBytes(),
-        new SegmentIndexEntry(ENTRY_1.key(), entryOffset1).getBytes()));
+        METADATA.getBytes(),
+        INDEX_ENTRY_0.getBytes(),
+        INDEX_ENTRY_1.getBytes()));
 
     assertThat(segmentIndex.getSegmentNumber()).isEqualTo(SEGMENT_NUMBER.value());
-    assertThat(segmentIndex.getKeyOffset(ENTRY_0.key())).hasValue(entryOffset0);
-    assertThat(segmentIndex.getKeyOffset(ENTRY_1.key())).hasValue(entryOffset1);
+    assertThat(segmentIndex.getKeyOffset(ENTRY_0.key())).hasValue(ENTRY_0_OFFSET);
+    assertThat(segmentIndex.getKeyOffset(ENTRY_1.key())).hasValue(ENTRY_1_OFFSET);
   }
 }
