e3d573eb8442f9335f33e835ffa39f4c9a760beb
Seth Butler <sbsoftware@protonmail.com>
2023-06-05 23:40:47 -0600

    storage: reduce compactor logging

--- storage/src/main/java/dev/sbutler/bitflask/storage/lsm/LSMTreeCompactor.java
+++ storage/src/main/java/dev/sbutler/bitflask/storage/lsm/LSMTreeCompactor.java
@@ -47,22 +47,20 @@ final class LSMTreeCompactor implements Runnable {
 
   @Override
   public void run() {
-    logger.atInfo().log("Starting compaction");
+    //    logger.atInfo().log("Starting compaction");
 
     Instant startInstant = Instant.now();
     if (flushMemtable()) {
       int numLevelsCompacted = compactSegmentLevels();
-      logger.atInfo()
-          .log("Flushed Memtable & compacted [%d] segment level(s) in [%d]ms", numLevelsCompacted,
-              Duration.between(startInstant, Instant.now()).toMillis());
-    } else {
-      logger.atInfo().log("Ending compaction without flushing Memtable");
+      logger.atInfo().log(
+          "Flushed Memtable & compacted [%d] segment level(s) in [%d]ms",
+          numLevelsCompacted, Duration.between(startInstant, Instant.now()).toMillis());
+      //    } else {
+      //      logger.atInfo().log("Ending compaction without flushing Memtable");
     }
   }
 
-  /**
-   * Returns true if the current {@link Memtable} was flushed to a {@link Segment}.
-   */
+  /** Returns true if the current {@link Memtable} was flushed to a {@link Segment}. */
   boolean flushMemtable() {
     try (var currentState = stateManager.getAndLockCurrentState()) {
       if (currentState.getMemtable().getNumBytesSize()
@@ -73,10 +71,8 @@ final class LSMTreeCompactor implements Runnable {
       SortedMap<String, Entry> flushedMemtable = currentState.getMemtable().flush();
       Segment segmentFromMemtable;
       try {
-        segmentFromMemtable = segmentFactory.create(
-            flushedMemtable,
-            0,
-            currentState.getMemtable().getNumBytesSize());
+        segmentFromMemtable =
+            segmentFactory.create(flushedMemtable, 0, currentState.getMemtable().getNumBytesSize());
       } catch (IOException e) {
         throw new StorageCompactionException("Failed to create new Segment from Memtable", e);
       }
@@ -89,17 +85,15 @@ final class LSMTreeCompactor implements Runnable {
       }
 
       // add memtable to segment level
-      SegmentLevelMultiMap newMultiMap = currentState.getSegmentLevelMultiMap().toBuilder()
-          .add(segmentFromMemtable)
-          .build();
+      SegmentLevelMultiMap newMultiMap =
+          currentState.getSegmentLevelMultiMap().toBuilder().add(segmentFromMemtable).build();
 
       // update state and release lock
       stateManager.updateCurrentState(newMemtable, newMultiMap);
 
-      logger.atInfo()
-          .log("Flushed Memtable with [%d] Entries to Segment [%d]",
-              flushedMemtable.size(),
-              segmentFromMemtable.getSegmentNumber());
+      logger.atInfo().log(
+          "Flushed Memtable with [%d] Entries to Segment [%d]",
+          flushedMemtable.size(), segmentFromMemtable.getSegmentNumber());
     }
     return true;
   }
@@ -118,11 +112,12 @@ final class LSMTreeCompactor implements Runnable {
     }
 
     int segmentLevel = 0;
-    for (; segmentLevelMultiMap.getNumBytesSizeOfSegmentLevel(segmentLevel)
-        >= getSegmentLevelFlushThreshold(segmentLevel);
+    for (;
+        segmentLevelMultiMap.getNumBytesSizeOfSegmentLevel(segmentLevel)
+            >= getSegmentLevelFlushThreshold(segmentLevel);
         segmentLevel++) {
-      segmentLevelMultiMap = segmentLevelCompactor
-          .compactSegmentLevel(segmentLevelMultiMap, segmentLevel);
+      segmentLevelMultiMap =
+          segmentLevelCompactor.compactSegmentLevel(segmentLevelMultiMap, segmentLevel);
     }
 
     // Only wait for lock if compaction occurred
