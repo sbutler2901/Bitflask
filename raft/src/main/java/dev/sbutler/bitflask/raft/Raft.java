c3c2a1f6848547d01f00146d22dd4b848e6d68a1
Seth Butler <sbsoftware@protonmail.com>
2023-06-19 22:59:09 -0600

    raft: stub Raft main library interface

--- raft/src/main/java/dev/sbutler/bitflask/raft/Raft.java
+++ raft/src/main/java/dev/sbutler/bitflask/raft/Raft.java
@@ -1,3 +1,22 @@
 package dev.sbutler.bitflask.raft;
 
-public class Raft {}
+import dev.sbutler.bitflask.raft.Entry.DeleteCommand;
+import dev.sbutler.bitflask.raft.Entry.SetCommand;
+
+/** The interface for using the Raft Consensus protocol. */
+public final class Raft {
+
+  /** A blocking call that returns once a command has successfully committed. */
+  public void commitCommand(SetCommand setCommand) {}
+
+  /** A blocking call that returns once a command has successfully committed. */
+  public void commitCommand(DeleteCommand deleteCommand) {}
+
+  public boolean isLeader() {
+    return false;
+  }
+  /** Return the current raft leader. */
+  public void getLeader() {
+    // TODO: establish and return server configuration
+  }
+}
