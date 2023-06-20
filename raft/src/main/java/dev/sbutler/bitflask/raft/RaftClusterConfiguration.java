67558c1642de9ff8063c792edc4c8b2faa77c962
Seth Butler <sbsoftware@protonmail.com>
2023-06-19 23:13:02 -0600

    raft: stub server and cluster configuration classes

---
+++ raft/src/main/java/dev/sbutler/bitflask/raft/RaftClusterConfiguration.java
@@ -0,0 +1,6 @@
+package dev.sbutler.bitflask.raft;
+
+import com.google.common.collect.ImmutableMap;
+
+/** The configuration for a Raft cluster. */
+public record RaftClusterConfiguration(ImmutableMap<RaftServerId, RaftServerInfo> clusterServers) {}
---
+++ raft/src/main/java/dev/sbutler/bitflask/raft/RaftServerId.java
@@ -0,0 +1,4 @@
+package dev.sbutler.bitflask.raft;
+
+/** The ID of a server in a raft cluster. */
+record RaftServerId(String id) {}
