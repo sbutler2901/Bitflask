78b1d79920b0ea42e90ac9e42cde86d27807e062
Seth Butler <sbsoftware@protonmail.com>
2023-06-19 23:17:42 -0600

    raft: add guice bindings for server and cluster configurations

--- raft/src/main/java/dev/sbutler/bitflask/raft/RaftModule.java
+++ raft/src/main/java/dev/sbutler/bitflask/raft/RaftModule.java
@@ -1,6 +1,27 @@
 package dev.sbutler.bitflask.raft;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
 
 /** Module for using the Raft Consensus protocol. */
-public class RaftModule extends AbstractModule {}
+public class RaftModule extends AbstractModule {
+
+  private final RaftServerInfo raftServerInfo;
+  private final RaftClusterConfiguration raftClusterConfiguration;
+
+  public RaftModule(
+      RaftServerInfo raftServerInfo, RaftClusterConfiguration raftClusterConfiguration) {
+    this.raftServerInfo = raftServerInfo;
+    this.raftClusterConfiguration = raftClusterConfiguration;
+  }
+
+  @Provides
+  RaftServerInfo provideRaftServerInfo() {
+    return raftServerInfo;
+  }
+
+  @Provides
+  RaftClusterConfiguration provideRaftClusterConfiguration() {
+    return raftClusterConfiguration;
+  }
+}
