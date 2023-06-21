68396ed4326203f8359469d2bcdd8708ca86f2a8
Seth Butler <sbsoftware@protonmail.com>
2023-06-20 23:32:58 -0600

    raft: stub RaftRequestProcessor and use it in the RaftService

--- raft/src/main/java/dev/sbutler/bitflask/raft/RaftService.java
+++ raft/src/main/java/dev/sbutler/bitflask/raft/RaftService.java
@@ -1,27 +1,53 @@
 package dev.sbutler.bitflask.raft;
 
+import com.google.rpc.Code;
+import com.google.rpc.Status;
+import io.grpc.StatusRuntimeException;
+import io.grpc.protobuf.StatusProto;
 import io.grpc.stub.StreamObserver;
 import jakarta.inject.Inject;
 
 /** The service implementing Raft RPC endpoints. */
 final class RaftService extends RaftGrpc.RaftImplBase {
 
+  private final RaftRequestProcessor raftRequestProcessor;
+
   @Inject
-  RaftService() {}
+  RaftService(RaftRequestProcessor raftRequestProcessor) {
+    this.raftRequestProcessor = raftRequestProcessor;
+  }
 
   @Override
   public void requestVote(
       RequestVoteRequest request, StreamObserver<RequestVoteResponse> responseObserver) {
-    RequestVoteResponse response = RequestVoteResponse.getDefaultInstance();
-    responseObserver.onNext(response);
-    responseObserver.onCompleted();
+    try {
+      RequestVoteResponse response = raftRequestProcessor.processRequestVoteRequest(request);
+      responseObserver.onNext(response);
+      responseObserver.onCompleted();
+    } catch (StatusRuntimeException e) {
+      responseObserver.onError(e);
+    } catch (Exception e) {
+      StatusRuntimeException statusRuntimeException =
+          StatusProto.toStatusRuntimeException(
+              Status.newBuilder().setCode(Code.INTERNAL_VALUE).setMessage(e.getMessage()).build());
+      responseObserver.onError(statusRuntimeException);
+    }
   }
 
   @Override
   public void appendEntries(
       AppendEntriesRequest request, StreamObserver<AppendEntriesResponse> responseObserver) {
-    AppendEntriesResponse response = AppendEntriesResponse.getDefaultInstance();
-    responseObserver.onNext(response);
-    responseObserver.onCompleted();
+    try {
+      AppendEntriesResponse response = raftRequestProcessor.processAppendEntriesRequest(request);
+      responseObserver.onNext(response);
+      responseObserver.onCompleted();
+    } catch (StatusRuntimeException e) {
+      responseObserver.onError(e);
+    } catch (Exception e) {
+      StatusRuntimeException statusRuntimeException =
+          StatusProto.toStatusRuntimeException(
+              Status.newBuilder().setCode(Code.INTERNAL_VALUE).setMessage(e.getMessage()).build());
+      responseObserver.onError(statusRuntimeException);
+    }
   }
 }
