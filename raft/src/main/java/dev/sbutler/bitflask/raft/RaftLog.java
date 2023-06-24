3c28614ac4f702db4c25fa7bcce2edd04a4db385
Seth Butler <sbsoftware@protonmail.com>
2023-06-24 01:08:59 -0600

    raft: add LastLogDetails to RaftLog

--- raft/src/main/java/dev/sbutler/bitflask/raft/RaftLog.java
+++ raft/src/main/java/dev/sbutler/bitflask/raft/RaftLog.java
@@ -1,4 +1,11 @@
 package dev.sbutler.bitflask.raft;
 
 /** Manages Raft's log of entries. */
-final class RaftLog {}
+final class RaftLog {
+
+  LastLogDetails getLastLogDetails() {
+    return new LastLogDetails(0, 0);
+  }
+
+  record LastLogDetails(long index, long term) {}
+}
