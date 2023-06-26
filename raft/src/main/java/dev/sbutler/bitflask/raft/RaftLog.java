d8fbc8dfbadce0d31e69fce1dbffd4cb7562b77e
Seth Butler <sbsoftware@protonmail.com>
2023-06-25 22:39:57 -0600

    raft: implement Comparable for LastLogDetails

--- raft/src/main/java/dev/sbutler/bitflask/raft/RaftLog.java
+++ raft/src/main/java/dev/sbutler/bitflask/raft/RaftLog.java
@@ -7,5 +7,20 @@ final class RaftLog {
     return new LastLogDetails(0, 0);
   }
 
-  record LastLogDetails(long index, long term) {}
+  record LastLogDetails(long term, long index) implements Comparable<LastLogDetails> {
+
+    @Override
+    public int compareTo(LastLogDetails provided) {
+      var termsCompared = Long.compare(term(), provided.term());
+      var indexesCompared = Long.compare(index(), provided.index());
+
+      if (termsCompared > 0) {
+        return 1;
+      }
+      if (termsCompared < 0) {
+        return -1;
+      }
+      return Integer.compare(indexesCompared, 0);
+    }
+  }
 }
