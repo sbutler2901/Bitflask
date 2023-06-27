f7d39d26b89889f5456e23e189766380d6b8b1fd
Seth Butler <sbsoftware@protonmail.com>
2023-06-26 23:27:16 -0600

    raft: stub method for appending entries to the raft log

--- raft/src/main/java/dev/sbutler/bitflask/raft/RaftLog.java
+++ raft/src/main/java/dev/sbutler/bitflask/raft/RaftLog.java
@@ -1,12 +1,30 @@
 package dev.sbutler.bitflask.raft;
 
+import jakarta.inject.Inject;
+import jakarta.inject.Singleton;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 /** Manages Raft's log of entries. */
+@Singleton
 final class RaftLog {
 
+  private final RaftVolatileState raftVolatileState;
+
   private final List<Entry> entries = new CopyOnWriteArrayList<>();
+
+  @Inject
+  RaftLog(RaftVolatileState raftVolatileState) {
+    this.raftVolatileState = raftVolatileState;
+  }
+
+  void appendEntriesWithLeaderCommit(List<Entry> newEntries, int leaderCommitIndex) {
+    // TODO: Handled appending entries, handling conflicts, and potentially applying
+    if (leaderCommitIndex > raftVolatileState.getHighestCommittedEntryIndex()) {
+      raftVolatileState.setHighestCommittedEntryIndex(
+          Math.min(leaderCommitIndex, getLastEntryIndex()));
+    }
+  }
 
   /**
    * Returns true if an entry exists at the provided index with a term matching the provided one.
@@ -19,12 +37,17 @@ final class RaftLog {
     return entry.getTerm() == term;
   }
 
+  private int getLastEntryIndex() {
+    return entries.size() - 1;
+  }
+
   LastLogEntryDetails getLastLogDetails() {
-    int lastEntryIndex = entries.size() - 1;
+    int lastEntryIndex = getLastEntryIndex();
     Entry lastEntry = entries.get(lastEntryIndex);
     return new LastLogEntryDetails(lastEntry.getTerm(), lastEntryIndex);
   }
 
+  /** Simplified details about the last {@link Entry} in the {@link RaftLog}. */
   record LastLogEntryDetails(int term, int index) implements Comparable<LastLogEntryDetails> {
 
     @Override
