906d65cabe8ff59cd51bccb88972a2de41f81dda
Seth Butler <sbsoftware@protonmail.com>
2023-06-26 22:02:17 -0600

    raft: implement more of RaftLog class

--- raft/src/main/java/dev/sbutler/bitflask/raft/RaftLog.java
+++ raft/src/main/java/dev/sbutler/bitflask/raft/RaftLog.java
@@ -1,10 +1,28 @@
 package dev.sbutler.bitflask.raft;
 
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
 /** Manages Raft's log of entries. */
 final class RaftLog {
 
+  private final List<Entry> entries = new CopyOnWriteArrayList<>();
+
+  /**
+   * Returns true if an entry exists at the provided index with a term matching the provided one.
+   */
+  boolean logAtIndexHasMatchingTerm(int index, int term) {
+    if (index >= entries.size()) {
+      return false;
+    }
+    Entry entry = entries.get(index);
+    return entry.getTerm() == term;
+  }
+
   LastLogDetails getLastLogDetails() {
-    return new LastLogDetails(0, 0);
+    int lastEntryIndex = entries.size() - 1;
+    Entry lastEntry = entries.get(lastEntryIndex);
+    return new LastLogDetails(lastEntry.getTerm(), lastEntryIndex);
   }
 
   record LastLogDetails(int term, int index) implements Comparable<LastLogDetails> {
