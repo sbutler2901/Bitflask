a90ef41996f5e22cf2ae98b9c4f81a1a4e1391c9
Seth Butler <sbsoftware@protonmail.com>
2023-06-28 00:01:10 -0600

    raft: create RaftRpcHandler interface

--- raft/src/main/java/dev/sbutler/bitflask/raft/RaftModeProcessor.java
+++ raft/src/main/java/dev/sbutler/bitflask/raft/RaftModeProcessor.java
@@ -1,14 +1,8 @@
 package dev.sbutler.bitflask.raft;
 
-/** Commons methods shared by all the various modes the Raft server can be operating in. */
-sealed interface RaftModeProcessor extends RaftElectionTimeoutHandler, Runnable
+/** Captures all capabilities that a Raft mode processor must provide. */
+sealed interface RaftModeProcessor extends RaftRpcHandler, RaftElectionTimeoutHandler, Runnable
     permits RaftModeProcessorBase {
-
-  /** Processes a {@link RequestVoteRequest} based on the current state of the server. */
-  RequestVoteResponse processRequestVoteRequest(RequestVoteRequest request);
-
-  /** Processes a {@link AppendEntriesRequest} based on the current state of the server. */
-  AppendEntriesResponse processAppendEntriesRequest(AppendEntriesRequest request);
 
   interface Factory {
 
