aa7b3090033998cdfd4b4e3a5a2316a50e4a701f
Seth Butler <sbsoftware@protonmail.com>
2023-06-29 20:28:22 -0600

    raft: RaftLeaderProcessor deny incoming RPCs unless it discovers a greater term.

--- raft/src/main/java/dev/sbutler/bitflask/raft/RaftLeaderProcessor.java
+++ raft/src/main/java/dev/sbutler/bitflask/raft/RaftLeaderProcessor.java
@@ -1,5 +1,8 @@
 package dev.sbutler.bitflask.raft;
 
+import com.google.rpc.Code;
+import com.google.rpc.Status;
+import io.grpc.protobuf.StatusProto;
 import jakarta.inject.Inject;
 
 /**
@@ -16,6 +19,24 @@ final class RaftLeaderProcessor extends RaftModeProcessorBase {
       RaftPersistentState raftPersistentState,
       RaftVolatileState raftVolatileState) {
     super(raftModeManager, raftPersistentState, raftVolatileState);
+  }
+
+  private void handleUnexpectedRequest() {
+    throw StatusProto.toStatusRuntimeException(
+        Status.newBuilder()
+            .setCode(Code.FAILED_PRECONDITION_VALUE)
+            .setMessage("This server is currently the leader and requests should not be sent to it")
+            .build());
+  }
+
+  @Override
+  protected void beforeProcessRequestVoteRequest(RequestVoteRequest request) {
+    handleUnexpectedRequest();
+  }
+
+  @Override
+  protected void beforeProcessAppendEntriesRequest(AppendEntriesRequest request) {
+    handleUnexpectedRequest();
   }
 
   @Override
