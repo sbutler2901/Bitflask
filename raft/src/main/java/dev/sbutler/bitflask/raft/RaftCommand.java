deaf80590cdc1addc989a244ade51920f61dc453
Seth Butler <sbsoftware@protonmail.com>
2023-07-03 14:27:13 -0600

    raft: create RaftCommand to unify types of commands supported by Raft internally and externally

---
+++ raft/src/main/java/dev/sbutler/bitflask/raft/RaftCommand.java
@@ -0,0 +1,9 @@
+package dev.sbutler.bitflask.raft;
+
+/** Commands that can be accepted and replicated. */
+public sealed interface RaftCommand {
+
+  record SetCommand(String key, String value) implements RaftCommand {}
+
+  record DeleteCommand(String key) implements RaftCommand {}
+}
