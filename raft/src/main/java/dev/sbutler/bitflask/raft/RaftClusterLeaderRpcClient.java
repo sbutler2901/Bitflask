269ee590d0e09c7bb9db5c81a9e784e51d3b4d07
Seth Butler <sbsoftware@protonmail.com>
2023-07-08 16:47:14 -0600

    raft: start implementation of RaftLeaderProcessor

--- raft/src/main/java/dev/sbutler/bitflask/raft/RaftClusterLeaderRpcClient.java
+++ raft/src/main/java/dev/sbutler/bitflask/raft/RaftClusterLeaderRpcClient.java
@@ -1,22 +1,122 @@
 package dev.sbutler.bitflask.raft;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.flogger.FluentLogger;
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.ListeningExecutorService;
 import dev.sbutler.bitflask.raft.RaftGrpc.RaftFutureStub;
+import java.util.concurrent.Future;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Predicate;
 
 /** Utility class for handling rpc calls used by the {@link RaftLeaderProcessor}. */
-final class RaftClusterLeaderRpcClient {
+final class RaftClusterLeaderRpcClient implements AutoCloseable {
 
   private static final FluentLogger logger = FluentLogger.forEnclosingClass();
 
   private final ListeningExecutorService executorService;
   private final ImmutableMap<RaftServerId, RaftFutureStub> otherServerStubs;
 
+  private final AtomicInteger responsesReceived = new AtomicInteger(0);
+  private final AtomicInteger successfulResponses = new AtomicInteger(0);
+  private final AtomicInteger largestTermSeen = new AtomicInteger(-1);
+
+  private ImmutableList<ListenableFuture<AppendEntriesResponse>> responseFutures =
+      ImmutableList.of();
+
   RaftClusterLeaderRpcClient(
       ListeningExecutorService executorService,
       ImmutableMap<RaftServerId, RaftFutureStub> otherServerStubs) {
     this.executorService = executorService;
     this.otherServerStubs = otherServerStubs;
+  }
+
+  void appendEntries(AppendEntriesRequest request) {
+    ImmutableList.Builder<ListenableFuture<AppendEntriesResponse>> responseFuturesBuilder =
+        ImmutableList.builder();
+    for (var stubsEntry : otherServerStubs.entrySet()) {
+      ListenableFuture<AppendEntriesResponse> responseFuture =
+          stubsEntry.getValue().appendEntries(request);
+      Futures.addCallback(
+          responseFuture, new AppendEntriesFutureCallback(stubsEntry.getKey()), executorService);
+    }
+    responseFutures = responseFuturesBuilder.build();
+  }
+
+  /**
+   * Gets the current results of all AppendEntries RPCs.
+   *
+   * <p>The returned {@link AppendEntriesResults} will vary until all responses have been received.
+   */
+  AppendEntriesResults getCurrentAppendEntriesResults() {
+    return new AppendEntriesResults(
+        responseFutures.size(),
+        responsesReceived.get(),
+        successfulResponses.getAndIncrement(),
+        largestTermSeen.get());
+  }
+
+  /** Cancels all pending requests, if any. */
+  @Override
+  public void close() {
+    responseFutures.stream()
+        .filter(Predicate.not(Future::isDone))
+        .filter(Predicate.not(Future::isCancelled))
+        .forEach(future -> future.cancel(true));
+  }
+
+  /**
+   * A simplified snapshot of all of {@link AppendEntriesResponse}s received from other servers in
+   * the cluster.
+   */
+  record AppendEntriesResults(
+      int numberRequestsSent,
+      int numberResponsesReceived,
+      int numberSuccessfulResponses,
+      int largestTermSeen) {
+
+    boolean allResponsesReceived() {
+      return numberRequestsSent() == numberResponsesReceived();
+    }
+
+    boolean majorityResponsesSuccessful() {
+      double requiredForMajority = numberRequestsSent() / 2.0;
+      return numberSuccessfulResponses() > requiredForMajority;
+    }
+
+    boolean allRequestsSuccessful() {
+      return numberRequestsSent() == numberSuccessfulResponses();
+    }
+  }
+
+  private final class AppendEntriesFutureCallback implements FutureCallback<AppendEntriesResponse> {
+
+    private final RaftServerId calledRaftServerId;
+
+    private AppendEntriesFutureCallback(RaftServerId calledRaftServerId) {
+      this.calledRaftServerId = calledRaftServerId;
+    }
+
+    @Override
+    public void onSuccess(AppendEntriesResponse result) {
+      responsesReceived.getAndIncrement();
+      largestTermSeen.getAndUpdate(current -> Math.max(current, result.getTerm()));
+      if (result.getSuccess()) {
+        successfulResponses.getAndIncrement();
+        logger.atInfo().log("Success received by [%s]", calledRaftServerId);
+      } else {
+        logger.atInfo().log("Failed received by [%s]", calledRaftServerId);
+      }
+    }
+
+    @Override
+    public void onFailure(Throwable t) {
+      //      responsesReceived.getAndIncrement();
+      logger.atWarning().withCause(t).log("Error received from [%s]", calledRaftServerId);
+      // TODO: handle infinite retry
+    }
   }
 }
--- raft/src/main/java/dev/sbutler/bitflask/raft/RaftLeaderProcessor.java
+++ raft/src/main/java/dev/sbutler/bitflask/raft/RaftLeaderProcessor.java
@@ -1,10 +1,16 @@
 package dev.sbutler.bitflask.raft;
 
-import static com.google.common.util.concurrent.Futures.immediateVoidFuture;
+import static com.google.common.util.concurrent.Futures.immediateFailedFuture;
 
+import com.google.common.flogger.FluentLogger;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.ListeningExecutorService;
+import com.google.common.util.concurrent.SettableFuture;
 import com.google.rpc.Code;
 import com.google.rpc.Status;
+import dev.sbutler.bitflask.raft.RaftClusterLeaderRpcClient.AppendEntriesResults;
 import dev.sbutler.bitflask.raft.RaftLog.LogEntryDetails;
+import dev.sbutler.bitflask.raft.exceptions.RaftException;
 import dev.sbutler.bitflask.raft.exceptions.RaftUnknownLeaderException;
 import io.grpc.protobuf.StatusProto;
 import jakarta.inject.Inject;
@@ -19,6 +25,9 @@ import java.util.concurrent.ConcurrentMap;
  */
 final class RaftLeaderProcessor extends RaftModeProcessorBase implements RaftCommandSubmitter {
 
+  private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+
+  private final ListeningExecutorService executorService;
   private final RaftLog raftLog;
   private final RaftClusterRpcChannelManager raftClusterRpcChannelManager;
   private final RaftCommandConverter raftCommandConverter;
@@ -31,11 +40,13 @@ final class RaftLeaderProcessor extends RaftModeProcessorBase implements RaftCom
       RaftModeManager raftModeManager,
       RaftPersistentState raftPersistentState,
       RaftVolatileState raftVolatileState,
+      ListeningExecutorService executorService,
       RaftLog raftLog,
       RaftClusterRpcChannelManager raftClusterRpcChannelManager,
       RaftCommandConverter raftCommandConverter,
       RaftClusterConfiguration raftClusterConfiguration) {
     super(raftModeManager, raftPersistentState, raftVolatileState);
+    this.executorService = executorService;
     this.raftLog = raftLog;
     this.raftClusterRpcChannelManager = raftClusterRpcChannelManager;
     this.raftCommandConverter = raftCommandConverter;
@@ -76,9 +87,45 @@ final class RaftLeaderProcessor extends RaftModeProcessorBase implements RaftCom
 
   @Override
   public RaftSubmitResults submitCommand(RaftCommand raftCommand) {
-    Entry newEntry = raftCommandConverter.convert(raftCommand);
-    raftLog.appendEntry(newEntry);
-    return new RaftSubmitResults.Success(immediateVoidFuture());
+    Entry newEntry;
+    try {
+      newEntry = raftCommandConverter.convert(raftCommand);
+      raftLog.appendEntry(newEntry);
+    } catch (RaftException e) {
+      logger.atSevere().withCause(e).log("Unable to create or append entry to log");
+      return new RaftSubmitResults.Success(immediateFailedFuture(e));
+    } catch (Exception e) {
+      logger.atSevere().withCause(e).log("Unable to create or append entry to log");
+      return new RaftSubmitResults.Success(
+          immediateFailedFuture(new RaftException("Unknown error while submitting.")));
+    }
+
+    SettableFuture<Void> clientSubmitFuture = SettableFuture.create();
+
+    ListenableFuture<Void> leaderSubmitFuture =
+        executorService.submit(
+            () -> {
+              AppendEntriesRequest request =
+                  createBaseAppendEntriesRequest().addEntries(newEntry).build();
+              try (RaftClusterLeaderRpcClient leaderRpcClient =
+                  raftClusterRpcChannelManager.createRaftClusterLeaderRpcClient()) {
+                leaderRpcClient.appendEntries(request);
+
+                AppendEntriesResults results = leaderRpcClient.getCurrentAppendEntriesResults();
+                while (!results.majorityResponsesSuccessful()) {
+                  results = leaderRpcClient.getCurrentAppendEntriesResults();
+                }
+
+                while (!results.allResponsesReceived()) {
+                  results = leaderRpcClient.getCurrentAppendEntriesResults();
+                }
+                if (!results.allRequestsSuccessful()) {
+                  // TODO: handle failed responses
+                }
+              }
+              return null;
+            });
+    return new RaftSubmitResults.Success(clientSubmitFuture);
   }
 
   /**
