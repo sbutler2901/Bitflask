f4c0cc025d59ac885b8259f31971cceb839c5736
Seth Butler <sbsoftware@protonmail.com>
2023-08-05 16:34:45 -0600

    all: remove jcommander dependency

--- config/src/main/java/dev/sbutler/bitflask/config/validators/NonBlankStringValidator.java
+++ config/src/main/java/dev/sbutler/bitflask/config/validators/NonBlankStringValidator.java
@@ -1,13 +1,12 @@
 package dev.sbutler.bitflask.config.validators;
 
-import com.beust.jcommander.ParameterException;
 import dev.sbutler.bitflask.config.InvalidConfigurationException;
 
 /** Validates that a string is non-blank according to {@link String#isBlank()}. */
 public class NonBlankStringValidator implements Validator<String> {
 
   @Override
-  public void validate(String name, String value) throws ParameterException {
+  public void validate(String name, String value) {
     if (value.isBlank()) {
       throw new InvalidConfigurationException(
           "Parameter " + name + " should not be blank (found [" + value + "])");
--- config/src/main/java/dev/sbutler/bitflask/config/validators/PositiveLongValidator.java
+++ config/src/main/java/dev/sbutler/bitflask/config/validators/PositiveLongValidator.java
@@ -1,13 +1,12 @@
 package dev.sbutler.bitflask.config.validators;
 
-import com.beust.jcommander.ParameterException;
 import dev.sbutler.bitflask.config.InvalidConfigurationException;
 
 /** Validates that a long is positive according to {@link Long#signum(long)}. */
 public class PositiveLongValidator implements Validator<Long> {
 
   @Override
-  public void validate(String name, Long value) throws ParameterException {
+  public void validate(String name, Long value) {
     if (Long.signum(value) < 1) {
       throw new InvalidConfigurationException(
           "Parameter " + name + " should be positive (found " + value + ")");
