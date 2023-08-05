834e3bc87332151b9480d694397688c11ae95078
Seth Butler <sbsoftware@protonmail.com>
2023-08-04 22:20:39 -0600

    config: create config package for managing Bitflask configurations

---
+++ config/src/main/java/dev/sbutler/bitflask/config/ConfigBuilder.java
@@ -0,0 +1,8 @@
+package dev.sbutler.bitflask.config;
+
+/** CLI tool for building BitflaskConfig as a json file. */
+public class ConfigBuilder {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
