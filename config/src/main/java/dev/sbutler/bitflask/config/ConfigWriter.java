d9e9a7e6c197ff37e82379e6f31272d0dc07cc81
Seth Butler <sbsoftware@protonmail.com>
2023-08-05 11:34:27 -0600

    config: add config writing

--- config/src/main/java/dev/sbutler/bitflask/config/ConfigWriter.java
+++ config/src/main/java/dev/sbutler/bitflask/config/ConfigWriter.java
@@ -1,16 +1,32 @@
 package dev.sbutler.bitflask.config;
 
 import com.google.common.collect.ImmutableMap;
+import com.google.common.flogger.FluentLogger;
+import com.google.protobuf.util.JsonFormat;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 /** CLI tool for building BitflaskConfig as a json file. */
-public class ConfigBuilder {
+public class ConfigWriter {
 
-  public static void main(String[] args) {
+  private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+
+  private static final String configFileName = "bitflask_config.json";
+
+  public static void main(String[] args) throws Exception {
     BitflaskConfig bitflaskConfig = buildBitflaskConfig();
-    Path configOutputPath = Paths.get(System.getProperty("user.home") + "/.bitflask/config");
-    // write to file
+    String configJson = JsonFormat.printer().print(bitflaskConfig);
+
+    Path configOutputDir = Paths.get(System.getProperty("user.home") + "/.bitflask/config");
+    Path configOutputFile = configOutputDir.resolve(configFileName);
+
+    Files.createDirectories(configOutputDir);
+    Files.writeString(configOutputFile, configJson, StandardCharsets.UTF_8);
+
+    logger.atInfo().log("Config writen to [%s]", configOutputFile.toAbsolutePath().toString());
+    logger.atInfo().log(configJson);
   }
 
   private static BitflaskConfig buildBitflaskConfig() {
