b994cea17bad479bd300e331b22dbaad60ca3291
Seth Butler <sbsoftware@protonmail.com>
2023-08-05 22:55:09 -0600

    raft: create RaftConfigurationProvider

---
+++ raft/src/main/java/dev/sbutler/bitflask/raft/RaftConfigurationProvider.java
@@ -0,0 +1,72 @@
+package dev.sbutler.bitflask.raft;
+
+import static com.google.mu.util.stream.GuavaCollectors.toImmutableMap;
+
+import com.google.common.base.Converter;
+import com.google.common.base.Suppliers;
+import com.google.common.collect.ImmutableMap;
+import com.google.mu.util.stream.BiStream;
+import dev.sbutler.bitflask.config.RaftConfig;
+import dev.sbutler.bitflask.config.ServerConfig;
+import jakarta.inject.Inject;
+import jakarta.inject.Provider;
+import java.util.function.Supplier;
+
+/**
+ * Handles providing a {@link RaftConfiguration} instance using data from {@link ServerConfig} and
+ * {@link RaftConfig}.
+ */
+final class RaftConfigurationProvider implements Provider<RaftConfiguration> {
+
+  private final ServerConfig serverConfig;
+  private final RaftConfig raftConfig;
+  private final Supplier<RaftConfiguration> raftConfiguration =
+      Suppliers.memoize(this::supplyRaftConfiguration);
+
+  @Inject
+  RaftConfigurationProvider(ServerConfig serverConfig, RaftConfig raftConfig) {
+    this.serverConfig = serverConfig;
+    this.raftConfig = raftConfig;
+  }
+
+  @Override
+  public RaftConfiguration get() {
+    return raftConfiguration.get();
+  }
+
+  private RaftConfiguration supplyRaftConfiguration() {
+    ImmutableMap<RaftServerId, RaftServerInfo> clusterServers =
+        BiStream.from(serverConfig.getBitflaskServersMap())
+            .mapKeys(RaftServerId::new)
+            .mapValues(ServerInfoConverter.INSTANCE::doForward)
+            .collect(toImmutableMap());
+    return new RaftConfiguration(
+        new RaftServerId(serverConfig.getThisServerId()),
+        clusterServers,
+        new RaftTimerInterval(
+            raftConfig.getTimerMinimumMilliseconds(), raftConfig.getTimerMaximumMilliseconds()));
+  }
+
+  private static class ServerInfoConverter
+      extends Converter<ServerConfig.ServerInfo, RaftServerInfo> {
+
+    private static final ServerInfoConverter INSTANCE = new ServerInfoConverter();
+
+    @Override
+    protected RaftServerInfo doForward(ServerConfig.ServerInfo serverInfo) {
+      return new RaftServerInfo(
+          new RaftServerId(serverInfo.getServerId()),
+          serverInfo.getHost(),
+          serverInfo.getRespPort());
+    }
+
+    @Override
+    protected ServerConfig.ServerInfo doBackward(RaftServerInfo raftServerInfo) {
+      return ServerConfig.ServerInfo.newBuilder()
+          .setServerId(raftServerInfo.id().toString())
+          .setHost(raftServerInfo.host())
+          .setRespPort(raftServerInfo.port())
+          .build();
+    }
+  }
+}
--- raft/src/main/java/dev/sbutler/bitflask/raft/RaftModule.java
+++ raft/src/main/java/dev/sbutler/bitflask/raft/RaftModule.java
@@ -3,38 +3,16 @@ package dev.sbutler.bitflask.raft;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.util.concurrent.Service;
 import com.google.inject.Injector;
-import com.google.inject.Provides;
 import com.google.inject.assistedinject.FactoryModuleBuilder;
-import dev.sbutler.bitflask.common.concurrency.VirtualThreadConcurrencyModule;
 import dev.sbutler.bitflask.common.guice.RootModule;
 
 /** Module for using the Raft Consensus protocol. */
 public class RaftModule extends RootModule {
 
-  private final RaftConfiguration raftConfiguration;
-  private final boolean standaloneMode;
-
-  private RaftModule(RaftConfiguration raftConfiguration, boolean standaloneMode) {
-    this.raftConfiguration = raftConfiguration;
-    this.standaloneMode = standaloneMode;
-  }
-
-  /** Creates a Raft module for usage within another application. */
-  public RaftModule create(RaftConfiguration raftConfiguration) {
-    return new RaftModule(raftConfiguration, false);
-  }
-
-  /** Creates a Raft module for operating in standalone mode. */
-  public RaftModule createStandalone(RaftConfiguration raftConfiguration) {
-    return new RaftModule(raftConfiguration, true);
-  }
-
   @Override
   protected void configure() {
     install(new FactoryModuleBuilder().build(RaftModeProcessor.Factory.class));
-    if (standaloneMode) {
-      install(new VirtualThreadConcurrencyModule());
-    }
+    bind(RaftConfiguration.class).toProvider(RaftConfigurationProvider.class);
   }
 
   @Override
@@ -43,15 +21,5 @@ public class RaftModule extends RootModule {
         injector.getInstance(RaftClusterRpcChannelManager.class),
         injector.getInstance(RaftEntryApplier.class),
         injector.getInstance(RaftServer.class));
-  }
-
-  @Provides
-  RaftConfiguration provideRaftClusterConfiguration() {
-    return raftConfiguration;
-  }
-
-  @Provides
-  RaftServer provideRaftServer(RaftServer raftServer) {
-    return raftServer;
   }
 }
